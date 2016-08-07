a0fcec7901a987346f39047540d29c3facfd0c1f
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-07 12:33:13 +0300

    Changes

--- vars/executeSteps.groovy
+++ vars/executeSteps.groovy
@@ -1,3 +1,8 @@
-def call(name) {
-    echo "Hello world, ${name}"
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    echo "Hello world, ${config.name}"
 }
