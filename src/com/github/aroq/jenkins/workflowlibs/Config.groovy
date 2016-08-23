330065dabfb3c92ee9e8586a55e7b1e3600e780c
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-23 19:30:37 +0300

    Changes

--- src/com/github/aroq/jenkins/workflowlibs/Config.groovy
+++ src/com/github/aroq/jenkins/workflowlibs/Config.groovy
@@ -1,8 +1,13 @@
 package com.github.aroq.jenkins.workflowlibs
 
-def readGroovyConfig(filePath) {
-    echo "filePath: ${filePath}"
-    config = ConfigSlurper.newInstance().parse(readFile(file: filePath))
-    echo config
+def readGroovyConfig(text) {
+    echo "text: ${text}"
+    config = groovyConfig(text)
     config
+}
+
+@NonCPS
+def groovyConfig(text) {
+    new groovy.json.JsonSlurper().parseText(json)
+    ConfigSlurper.newInstance().parse(text)
 }
