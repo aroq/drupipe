10816c1887eaa14c35e02f5f9925f0f74048fa36
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-24 13:09:58 +0300

    Changes

--- vars/initStage.groovy
+++ vars/initStage.groovy
@@ -2,10 +2,11 @@ def call(body) {
     def params = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = params
-    params << params.params
+//    params << params.params
     body()
 
     stage 'init'
+    echo params
 
     dir('library') {
         git url: 'https://github.com/aroq/jenkins-pipeline-library.git', branch: 'master'
