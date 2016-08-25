eec7286924186078b2601a7fd8505267718ec5e5
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-25 14:23:04 +0300

    Changes

---
+++ vars/colorEcho.groovy
@@ -0,0 +1,4 @@
+def call(message, color = null) {
+    utils = new com.github.aroq.workflowlibs.Utils()
+    utils.colorEcho(message, color)
+}
--- vars/dump.groovy
+++ vars/dump.groovy
@@ -1,17 +1,14 @@
-def call(params, String dumpName = null) {
-    utils = new com.github.aroq.workflowlibs.Utils()
-    if (dumpName) {
-        utils.colorEcho "Dumping ${dumpName} values:"
-    }
-
-    utils.colorEcho collectParams(params)
+def call(params, String dumpName = '') {
+//    utils = new com.github.aroq.workflowlibs.Utils()
+    colorEcho "Dumping ${dumpName} values:"
+    colorEcho collectParams(params)
 }
 
 @NonCPS
 def collectParams(params) {
     def String result = 'params - '
     for (item in params) {
-        result += "${item.key} = ${item.value}"
+        result = result + "${item.key} = ${item.value}\r\n"
     }
     result
 }
