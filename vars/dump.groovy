83406c69ab41680a159f21955a45ccd2a7141542
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-25 13:44:33 +0300

    Changes

--- vars/dump.groovy
+++ vars/dump.groovy
@@ -1,7 +1,7 @@
-def call(params, $name = nil) {
-    if ($name) {
-        echo "Dumping ${name} values:"
-    }
+def call(params) {
+//    if ($name) {
+//        echo "Dumping ${name} values:"
+//    }
     for (item in params) {
         echo "${item.key} = ${item.value}"
     }
