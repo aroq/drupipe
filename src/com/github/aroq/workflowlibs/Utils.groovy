61e952878a0abbc45c680791dc79cef232db13ae
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-25 14:41:51 +0300

    Changes

--- src/com/github/aroq/workflowlibs/Utils.groovy
+++ src/com/github/aroq/workflowlibs/Utils.groovy
@@ -1,8 +1,34 @@
 package com.github.aroq.workflowlibs
 
 def colorEcho(message, color = null) {
+    if (!color) {
+        color = 'green'
+    }
+    else {
+        switch (color) {
+            case 'red':
+                color = 31
+                break
+            case 'green':
+                color = 32
+                break
+            case 'yellow':
+                color = 33
+                break
+            case 'blue':
+                color = 34
+                break
+            case 'magenta':
+                color = 35
+                break
+            case 'cyan':
+                color = 36
+                break
+        }
+    }
+
     wrap([$class: 'AnsiColorBuildWrapper']) {
-        echo "\u001B[31m${message}\u001B[0m"
+        echo "\u001B[${color}m${message}\u001B[0m"
     }
 }
 
--- vars/dump.groovy
+++ vars/dump.groovy
@@ -1,12 +1,11 @@
 def call(params, String dumpName = '') {
-//    utils = new com.github.aroq.workflowlibs.Utils()
     colorEcho "Dumping ${dumpName} values:"
     colorEcho collectParams(params)
 }
 
 @NonCPS
 def collectParams(params) {
-    def String result = 'params - '
+    def String result
     for (item in params) {
         result = result + "${item.key} = ${item.value}\r\n"
     }
