3dcd9e716d0ce617e92938b3712bfdb7a7829c34
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-25 23:26:13 +0300

    Changes

--- vars/executeStage.groovy
+++ vars/executeStage.groovy
@@ -13,16 +13,16 @@ def call(name, body) {
             def values = action.split("\\.")
             def actionInstance = this.class.classLoader.loadClass("com.github.aroq.workflowlibs.${values[0]}", true, false )?.newInstance()
             def methodName = values[1]
-            dump(params, "Action params")
+            dump(params, "${action} action params")
             actionResult = actionInstance."$methodName"(params)
             if (actionResult) {
                result << actionResult
             }
-            dump(result, "Action result")
+            dump(result, "${action} action result")
             result
         }
         catch (err) {
-            echo "Action ${action} is not exists."
+            echo "Action ${action} is not exists or error in action."
             echo err.toString()
             throw err
         }
