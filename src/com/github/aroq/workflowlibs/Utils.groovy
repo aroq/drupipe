9c7876ed94909b4e9d7275cc91a70f7dba7307b3
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-25 14:43:54 +0300

    Changes

--- src/com/github/aroq/workflowlibs/Utils.groovy
+++ src/com/github/aroq/workflowlibs/Utils.groovy
@@ -4,27 +4,25 @@ def colorEcho(message, color = null) {
     if (!color) {
         color = 'green'
     }
-    else {
-        switch (color) {
-            case 'red':
-                color = 31
-                break
-            case 'green':
-                color = 32
-                break
-            case 'yellow':
-                color = 33
-                break
-            case 'blue':
-                color = 34
-                break
-            case 'magenta':
-                color = 35
-                break
-            case 'cyan':
-                color = 36
-                break
-        }
+    switch (color) {
+        case 'red':
+            color = 31
+            break
+        case 'green':
+            color = 32
+            break
+        case 'yellow':
+            color = 33
+            break
+        case 'blue':
+            color = 34
+            break
+        case 'magenta':
+            color = 35
+            break
+        case 'cyan':
+            color = 36
+            break
     }
 
     wrap([$class: 'AnsiColorBuildWrapper']) {
