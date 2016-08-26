7e629d104f1e96e370c35d12c5e2e6ca18fc4391
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-26 14:35:31 +0300

    Changes

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -5,19 +5,29 @@ def call(body) {
     body()
 
     node {
-//        HashMap pipeline = [
-//                'init' : ['Config.perform'],
-//                'build': ['Docman.deploy', 'Docman.info'],
-//                'ops'  : ['Druflow.deployFlow']
-//        ]
-        pipeline = jsonParse('{"init": ["Config.perform"], "build": ["Docman.deploy", "Docman.info"]}')
+        HashMap pipeline = [
+                'init' : ['Config.perform'],
+                'build': ['Docman.deploy', 'Docman.info'],
+                'ops'  : ['Druflow.deployFlow']
+        ]
 
-        for (int i = 0; i < pipeline.size(); i++) {
-            params.stage = pipeline[i]
-            params << executeStage(pipeline[i].name) {
+        pp = pipelineParse(pipeline)
+
+        for (int i = 0; i < pp.size(); i++) {
+            params.stage = pp[i]
+            params << executeStage(pp[i].name) {
                 p = params
             }
         }
+
+        jsonPipeline = jsonParse('{"init": ["Config.perform"], "build": ["Docman.deploy", "Docman.info"]}')
+
+//        for (int i = 0; i < jsonPipeline.size(); i++) {
+//            params.stage = jsonPipeline[i]
+//            params << executeStage(jsonPipeline[i].name) {
+//                p = params
+//            }
+//        }
 
 //        params << executeStage('init') {
 //            p = params
@@ -33,6 +43,18 @@ def call(body) {
 //            p = params
 //            actions = ['Druflow.deployFlow']
 //        }
+    }
+}
+
+@NonCPS
+def pipelineParse(pipeline) {
+    List<com.github.aroq.workflowlibs.Stage> result = []
+    for (item in pipeline) {
+//        actions = []
+//        for (action in item.value) {
+//            actions << action
+//        }
+        result << new com.github.aroq.workflowlibs.Stage(name: item.key, actionList: ['Config.perform'])
     }
 }
 
