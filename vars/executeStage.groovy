cc4ccfc2fc3590bc9fea3cc5a3aed15f098f22d4
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-26 11:35:24 +0300

    Changes

--- vars/executeStage.groovy
+++ vars/executeStage.groovy
@@ -14,12 +14,12 @@ def call(name, body) {
             def values = action.split("\\.")
             def actionInstance = this.class.classLoader.loadClass("com.github.aroq.workflowlibs.${values[0]}", true, false )?.newInstance()
             def methodName = values[1]
-            dump(params, "${action} action params")
+//            dump(params, "${action} action params")
             actionResult = actionInstance."$methodName"(params)
             if (actionResult) {
-                params << actionResult
+//                params << actionResult
             }
-            dump(params, "${action} action result")
+//            dump(params, "${action} action result")
         }
         params.remove('actions')
         params
