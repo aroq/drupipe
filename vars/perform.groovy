215d1903dba42231b56def57118cb227474ada3d
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-26 18:32:26 +0300

    Changes

--- vars/perform.groovy
+++ vars/perform.groovy
@@ -6,7 +6,27 @@ def call(body) {
 
     node {
         config = executePipeline {
-            pipeline = ['init' : [[action: 'Config.perform', params: [param1: 'test2']]]]
+            pipeline =
+                [
+                        'init':
+                                [
+                                        [
+                                                action: 'Config.perform',
+                                                params: [param1: 'test2']
+                                        ]
+                                ],
+                        'build':
+                                [
+                                        [
+                                                action: 'Docman.deploy',
+                                                params: [param1: 'test2']
+                                        ],
+                                        [
+                                                action: 'Docman.info',
+                                                params: [param1: 'test2']
+                                        ],
+                                ]
+                ]
             p = params
         }
         dump(config)
