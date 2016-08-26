137ed5f1389e142c5f7086825dffe592af48c671
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-26 17:39:05 +0300

    Changes

--- vars/executePipeline.groovy
+++ vars/executePipeline.groovy
@@ -36,7 +36,7 @@ def processPipeline(pipeline) {
         List<String> actions = []
 
         for (action in item.value) {
-            if (item.value.getClass == String) {
+            if (item.value.getClass() == String) {
                 actions << action
             }
             else {
