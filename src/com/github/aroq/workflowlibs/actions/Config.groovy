d08fb0a45cae83605f0bc97092c3f840f9449720
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-27 14:21:15 +0300

    Changes

--- src/com/github/aroq/workflowlibs/actions/Config.groovy
+++ src/com/github/aroq/workflowlibs/actions/Config.groovy
@@ -9,24 +9,13 @@ def perform(params) {
     config << params
 
     if (params.configProviders) {
-        echo "Config providers class: ${params.configProviders.getClass()}"
-        echo "Config providers size: ${params.configProviders.size()}"
         for (int i = 0; i < params.configProviders.size(); i++) {
-            echo "test"
             action = utils.processPipelineAction(params.configProviders[i])
-            echo "Action class: ${action.getClass()}"
             params << executeAction(action) {
                 p = params
             }
-//            if (params.configProviders[i].name == 'docman') {
-//                action = utils.processPipelineAction([action: 'Docman.info'])
-//                params << executeAction(action) {
-//                    p = params
-//                }
-//            }
         }
     }
-
 
     config << params
     config
