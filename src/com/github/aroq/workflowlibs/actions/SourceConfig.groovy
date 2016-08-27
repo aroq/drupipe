791a869e4b9fd2c93a44fc96fe18c7c158f6f6aa
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-27 21:24:22 +0300

    Changes

--- src/com/github/aroq/workflowlibs/actions/SourceConfig.groovy
+++ src/com/github/aroq/workflowlibs/actions/SourceConfig.groovy
@@ -1,7 +1,9 @@
 package com.github.aroq.workflowlibs.actions
 
 def perform(params) {
+    echo "SourceConfig: start"
     configFilePath = sourcePath(params, params.sourceName, params.sourcePath)
+    echo "SourceConfig: Config file path: ${configFilePath}"
     utils = new com.github.aroq.workflowlibs.Utils()
 
     if (params.sourceType == 'groovy') {
