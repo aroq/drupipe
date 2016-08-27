6c83f80cc61c711d0af71a1ceb86dc41238a5391
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-27 20:31:40 +0300

    Changes

--- vars/sourcePath.groovy
+++ vars/sourcePath.groovy
@@ -1,4 +1,6 @@
 def call(params, sourceName, String path) {
+    echo "Source name: ${sourceName}"
+    dump(params, 'sourcePath')
     if (sourceName in params.sources) {
         params.sources[sourceName]['path'] + '/' + path
     }
