0979f2482b655af132178dbbfd5689256ad27a6a
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-27 15:22:43 +0300

    Changes

--- src/com/github/aroq/workflowlibs/actions/Config.groovy
+++ src/com/github/aroq/workflowlibs/actions/Config.groovy
@@ -13,6 +13,8 @@ def perform(params) {
     providers << [action: 'GroovyFileConfig.perform', params: [configFileName: 'library/config/config.groovy']]
     providers << params.configProviders
 
+    dump(config, "before action list")
+
     for (int i = 0; i < providers.size(); i++) {
         action = utils.processPipelineAction(providers[i])
         params << executeAction(action) {
