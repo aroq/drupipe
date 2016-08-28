25afb37207f9ea3a68c63abf99070f2a297fd83b
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-28 14:48:28 +0300

    Changes

--- src/com/github/aroq/workflowlibs/actions/Config.groovy
+++ src/com/github/aroq/workflowlibs/actions/Config.groovy
@@ -3,11 +3,32 @@ package com.github.aroq.workflowlibs.actions
 def perform(params) {
     params.workspace = pwd()
 
-    def providers = []
-    source = [name: 'library', type: 'git', path: 'library', url: 'https://github.com/aroq/jenkins-pipeline-library.git', branch: 'master']
-    providers << [action: 'Source.add', params: [source: source]]
-    providers << [action: 'Source.loadConfig', params: [sourceName: 'library', configType: 'groovy', configPath: 'config/config.groovy']]
-    providers << params.configProviders
+    def providers = [
+        [
+            action: 'Source.add',
+            params: [
+                source: [
+                    name: 'library',
+                    type: 'git',
+                    url: 'https://github.com/aroq/jenkins-pipeline-library.git',
+                    path: 'library',
+                    branch: 'master',
+                ]
+            ]
+        ],
+        [
+            action: 'Source.loadConfig',
+            params: [
+                sourceName: 'library',
+                configType: 'groovy',
+                configPath: 'config/config.groovy'
+            ]
+        ],
+    ] << params.configProviders
+//    source = [name: 'library', type: 'git', path: 'library', url: 'https://github.com/aroq/jenkins-pipeline-library.git', branch: 'master']
+//    providers << [action: 'Source.add', params: [source: source]]
+//    providers << [action: 'Source.loadConfig', params: [sourceName: 'library', configType: 'groovy', configPath: 'config/config.groovy']]
+//    providers << params.configProviders
     jsonDump(providers, 'config providers')
 
     params << executePipelineActionList(providers) {
