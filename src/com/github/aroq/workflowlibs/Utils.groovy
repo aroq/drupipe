08d602b5dcbb7236af6a21ec74b067daea93dbab
Alexander Tolstikov <atolstikov@adyax.com>
2016-08-28 13:35:48 +0300

    Changes

--- src/com/github/aroq/workflowlibs/Utils.groovy
+++ src/com/github/aroq/workflowlibs/Utils.groovy
@@ -63,9 +63,8 @@ def processPipelineAction(action) {
     new com.github.aroq.workflowlibs.Action(name: values[0], methodName: values[1], params: actionParams)
 }
 
-@nonCPS
+@NonCPS
 def processSources(sources) {
-
 
 }
 
