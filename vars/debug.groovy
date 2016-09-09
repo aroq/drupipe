61ff1b4096704b49a927d966d8d5a96c86a786e5
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-10 00:40:47 +0300

    Changes

--- vars/debug.groovy
+++ vars/debug.groovy
@@ -1,18 +1,18 @@
 import org.codehaus.groovy.runtime.GStringImpl
 
 def call(params, value, String dumpName = '', debugParams = [:]) {
-    if (params.debugEnabled && value) {
-        if (value instanceof java.lang.String || value instanceof GStringImpl) {
-//            echo "${dumpName}: ${value}"
-        }
-        else {
+//    if (params.debugEnabled && value) {
+//        if (value instanceof java.lang.String || value instanceof GStringImpl) {
+////            echo "${dumpName}: ${value}"
+//        }
+//        else {
 //            if (debugParams?.debugMode == 'json' || params.debugMode == 'json') {
 ////                jsonDump(value, dumpName)
 //            }
 //            else {
 ////                dump(value, dumpName)
 //            }
-        }
-    }
+//        }
+//    }
 }
 
