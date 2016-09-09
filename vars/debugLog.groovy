8107256b0bc7a67bd2be3f2302d4a25b099ad191
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-10 01:00:42 +0300

    Changes

--- vars/debugLog.groovy
+++ vars/debugLog.groovy
@@ -1,9 +1,13 @@
 def call(params, value, dumpName = '', debugParams = [:]) {
+    echo "debugLog"
     if (params.debugEnabled) {
+        echo "debugEnabled"
         if (value instanceof java.lang.String) {
+            echo "debugEnabledString"
             echo "${dumpName}: ${value}"
         }
         else {
+            echo "debugEnabledNotString"
             if (debugParams?.debugMode == 'json' || params.debugMode == 'json') {
                 jsonDump(value, dumpName)
             }
