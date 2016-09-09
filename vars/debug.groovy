d310b030ebd8855555ca339323736133d0258575
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-09 22:55:36 +0300

    Changes

--- vars/debug.groovy
+++ vars/debug.groovy
@@ -1,4 +1,4 @@
-def call(params, value, String dumpName = '', debugParams = '') {
+def call(params, value, String dumpName = '', debugParams = [:]) {
     if (params.debug) {
         if (value instanceof java.lang.String) {
             echo "${dumpName}: ${value}"
