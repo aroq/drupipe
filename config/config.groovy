61d86528fc29c4d9dc51b6bf2b42091a5b937a8c
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-09 22:33:49 +0300

    Changes

--- config/config.groovy
+++ config/config.groovy
@@ -1,4 +1,4 @@
-debug = false
+debug = true
 docrootDir = 'docroot'
 commandParams = [
     JobDslSeed: [
--- vars/sourcePath.groovy
+++ vars/sourcePath.groovy
@@ -1,6 +1,6 @@
 def call(params, sourceName, String path) {
     echo "Source name: ${sourceName}"
-    dump(params, 'sourcePath')
+    debug(params, 'sourcePath')
     if (sourceName in params.sources) {
         params.sources[sourceName].path + '/' + path
     }
