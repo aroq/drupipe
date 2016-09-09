27014927d8c2a2c045968eaa82e3372a22ddd54e
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-09 22:07:50 +0300

    Changes

--- src/com/github/aroq/workflowlibs/actions/JobDslSeed.groovy
+++ src/com/github/aroq/workflowlibs/actions/JobDslSeed.groovy
@@ -1,12 +1,13 @@
 package com.github.aroq.workflowlibs.actions
 
 def perform(params) {
-    defaultParams = [
-        removedJobAction: 'DELETE',
-        removedViewAction: 'DELETE',
-        lookupStrategy: 'SEED_JOB',
-        additionalClasspath: ['library/src']
-    ]
+//    defaultParams = [
+//        removedJobAction: 'DELETE',
+//        removedViewAction: 'DELETE',
+//        lookupStrategy: 'SEED_JOB',
+//        additionalClasspath: ['library/src']
+//    ]
+    defaultParams = params.commandParams[params.action.name]
     params << defaultParams << params
 
     jobDsl targets: [params.jobsPattern].join('\n'),
