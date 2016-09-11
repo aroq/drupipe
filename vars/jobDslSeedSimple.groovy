f1674639ea7b3f13c1ac28d1b9f7945ad574a77c
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-11 23:13:13 +0300

    Changes

---
+++ vars/jobDslSeedSimple.groovy
@@ -0,0 +1,42 @@
+def call(body) {
+    def params = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = params
+    body()
+
+    executePipeline {
+        checkoutSCM = true
+        pipeline = [
+            'init': [
+                [
+                    action: 'Source.add',
+                    params: [
+                        source: [
+                            name: 'config',
+                            type: 'dir',
+                            path: '',
+                        ]
+                    ]
+                ],
+                [
+                    action: 'Config.perform',
+                    params: [
+                        configProviders: [
+                            action: 'Source.loadConfig',
+                            params: [
+                                sourceName: 'config',
+                                configType: 'groovy',
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            'seed': [
+//                [
+//                    action: 'JobDslSeed.perform',
+//                ],
+            ],
+        ]
+        p = params
+    }
+}
