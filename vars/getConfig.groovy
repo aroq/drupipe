35bc9c64ea82b99b2c677898f204e19d17f5102e
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-15 16:34:17 +0300

    Changes

--- vars/getConfig.groovy
+++ vars/getConfig.groovy
@@ -5,13 +5,18 @@ import com.github.aroq.workflowlibs.ConfigContainer
 @Field
 ConfigContainer configContainer
 
+@Field
+Integer callCount = 0
+
 def call() {
+    callCount++
+    echo "Call count: ${callCount}"
     echo "getConfig()"
     jsonDump(this.configContainer, 'before creation')
     if (!this.configContainer) {
         echo "Create new Config Container"
         this.configContainer = new ConfigContainer(params: [asfasdf: 'asdfadfa'])
-        this.configContainer.addParams([test2: 'test2'])
+        this.configContainer.params = [test2: 'test2']
         jsonDump(this.configContainer, 'after creation')
     }
     return this.configContainer
