fe34f11572ea7bad250ede28c256be5d9ef2aad7
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-16 17:22:28 +0300

    Changes

--- vars/pipelineTriggerDocman.groovy
+++ vars/pipelineTriggerDocman.groovy
@@ -16,5 +16,7 @@ def call(body) {
         echo "gitlabSourceRepoName: ${env.gitlabSourceRepoName}"
         echo "gitlabSourceNamespace: ${env.gitlabSourceNamespace}"
         echo "gitlabSourceRepoURL: ${env.gitlabSourceRepoURL}"
+
+        build job: 'development', parameters: [string(name: 'executeCommand', value: 'deployFlow'), string(name: 'projectName', value: 'common'), string(name: 'environment', value: 'dev'), string(name: 'debug', value: '0'), string(name: 'simulate', value: '0'), string(name: 'docrootDir', value: 'docroot'), string(name: 'config_repo', value: 'http://gitlab/drucon2016/config.git'), string(name: 'type', value: 'branch'), string(name: 'version', value: 'develop'), string(name: 'force', value: '0'), string(name: 'skip_stage_build', value: '0'), string(name: 'skip_stage_operations', value: '0'), string(name: 'skip_stage_test', value: '0')]
     }
 }
