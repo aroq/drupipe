bf72ab0ce051af438abc18d9cce84d1b35b55a50
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-16 15:17:35 +0300

    Changes

--- jobdsl/job_dsl_docman.groovy
+++ jobdsl/job_dsl_docman.groovy
@@ -62,6 +62,7 @@ docmanConfig.states?.each { state ->
     }
 }
 
+
 // Create trigger jobs for each project & state defined in Docman config.
 docmanConfig.projects?.each { project ->
     if (project.value['repo'] && project.value['trigger'] != false) {
