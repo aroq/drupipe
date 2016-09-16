67e23d23700836296a99179fa2b0b4b4ffe7aa2e
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-17 00:05:40 +0300

    Changes

--- src/com/github/aroq/workflowlibs/actions/Druflow.groovy
+++ src/com/github/aroq/workflowlibs/actions/Druflow.groovy
@@ -4,7 +4,7 @@ def deployFlow(params) {
     debugLog(params, params, 'Deploy Flow')
     dir('druflow') {
         git 'https://github.com/aroq/druflow.git'
-        sh "./gradlew app -Ddebug=${debug} -DprojectName=${projectName} -Denv=${environment} -DexecuteCommand=${executeCommand} -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir}"
+        sh "./gradlew app -Ddebug=${debug} -DprojectName=${projectName} -Denv=${environment} -DexecuteCommand=${executeCommand} -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir} -Dtag='stable-2016-09-16-21-00-55'"
     }
 }
 
