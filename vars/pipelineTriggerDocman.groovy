4233978025c83fdb4675cd5767eefadf02af41a7
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-16 18:47:57 +0300

    Changes

--- vars/pipelineTriggerDocman.groovy
+++ vars/pipelineTriggerDocman.groovy
@@ -5,54 +5,48 @@ def call(body) {
     body()
 
     node {
-//        properties(
-//            [
-//                [$class: 'GitLabConnectionProperty', gitLabConnection: 'Gitlab'],
-//            ]
-//        )
-        properties [pipelineTriggers([]), buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '30')), disableConcurrentBuilds(), [$class: 'GitLabConnectionProperty', gitLabConnection: 'Gitlab'], [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false]]
+        properties[pipelineTriggers([]), buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '30')), disableConcurrentBuilds(), [$class: 'GitLabConnectionProperty', gitLabConnection: 'Gitlab'], [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false]]
 
         stage 'trigger'
+        gitlabCommitStatus {
+            echo "gitlabSourceBranch: ${env.gitlabSourceBranch}"
+            echo "gitlabSourceRepoName: ${env.gitlabSourceRepoName}"
+            echo "gitlabSourceNamespace: ${env.gitlabSourceNamespace}"
+            echo "gitlabSourceRepoURL: ${env.gitlabSourceRepoURL}"
 
-        echo "gitlabSourceBranch: ${env.gitlabSourceBranch}"
-        echo "gitlabSourceRepoName: ${env.gitlabSourceRepoName}"
-        echo "gitlabSourceNamespace: ${env.gitlabSourceNamespace}"
-        echo "gitlabSourceRepoURL: ${env.gitlabSourceRepoURL}"
+            // TODO: Use docman config to set params.
+            switch (env.gitlabSourceBranch) {
+                case 'develop':
+                    buildJob = 'development'
+                    buildEnvironment = 'dev'
+                    buildVersionType = 'branch'
+                    break
+                case 'master':
+                    buildJob = 'staging'
+                    buildEnvironment = 'test'
+                    buildVersionType = 'branch'
+                    break
+                case 'state_stable':
+                    buildJob = 'stable'
+                    buildEnvironment = 'prod'
+                    buildVersionType = 'tag'
+                    break
+            }
 
-        dump(params, 'Params')
-
-        // TODO: Use docman config to set params.
-        switch (env.gitlabSourceBranch) {
-            case 'develop':
-                buildJob = 'development'
-                buildEnvironment = 'dev'
-                buildVersionType = 'branch'
-                break
-            case 'master':
-                buildJob = 'staging'
-                buildEnvironment = 'test'
-                buildVersionType = 'branch'
-                break
-            case 'state_stable':
-                buildJob = 'stable'
-                buildEnvironment = 'prod'
-                buildVersionType = 'tag'
-                break
+            build job: buildJob, parameters: [
+                string(name: 'executeCommand', value: 'deployFlow'),
+                string(name: 'projectName', value: env.gitlabSourceRepoName),
+                string(name: 'environment', value: buildEnvironment),
+                string(name: 'debug', value: '0'),
+                string(name: 'simulate', value: '0'),
+                string(name: 'docrootDir', value: 'docroot'),
+                string(name: 'config_repo', value: params.configRepo),
+                string(name: 'type', value: buildVersionType),
+                string(name: 'version', value: env.gitlabSourceBranch),
+                string(name: 'force', value: '0'),
+                string(name: 'skip_stage_build', value: '0'),
+                string(name: 'skip_stage_operations', value: '0'),
+                string(name: 'skip_stage_test', value: '0')]
         }
-
-        build job: buildJob, parameters: [
-            string(name: 'executeCommand', value: 'deployFlow'),
-            string(name: 'projectName', value: env.gitlabSourceRepoName),
-            string(name: 'environment', value: buildEnvironment),
-            string(name: 'debug', value: '0'),
-            string(name: 'simulate', value: '0'),
-            string(name: 'docrootDir', value: 'docroot'),
-            string(name: 'config_repo', value: params.configRepo),
-            string(name: 'type', value: buildVersionType),
-            string(name: 'version', value: env.gitlabSourceBranch),
-            string(name: 'force', value: '0'),
-            string(name: 'skip_stage_build', value: '0'),
-            string(name: 'skip_stage_operations', value: '0'),
-            string(name: 'skip_stage_test', value: '0')]
     }
 }
