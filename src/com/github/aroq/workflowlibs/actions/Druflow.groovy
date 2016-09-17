8bb20b569d88c4779cf2091e4322717c284b23de
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-17 13:07:40 +0300

    Changes

--- src/com/github/aroq/workflowlibs/actions/Druflow.groovy
+++ src/com/github/aroq/workflowlibs/actions/Druflow.groovy
@@ -3,17 +3,13 @@ package com.github.aroq.workflowlibs.actions
 def deployFlow(params) {
     debugLog(params, params, 'Deploy Flow')
 
-    options = ' '
+    options = ''
     if (fileExists(file: params.propertiesFile)) {
-        props = readProperties file: params.propertiesFile
-        if (props.tag) {
-            options += "-Dtag=${props.tag}"
-        }
-        echo "Properties: ${getOptions(props)}"
+        options = getOptions(readProperties(file: params.propertiesFile))
     }
     dir('druflow') {
         git 'https://github.com/aroq/druflow.git'
-        sh "./gradlew app -Ddebug=${debug} -DprojectName=${projectName} -Denv=${environment} -DexecuteCommand=${executeCommand} -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir} ${options}"
+        sh "./gradlew app -Ddebug=${debug} -DprojectName=${projectName} -Denv=${environment} -DexecuteCommand=${executeCommand} -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir}${options}"
     }
 }
 
