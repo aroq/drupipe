32916b73ae80b7ee31f0916bac9f6208c93e5425
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-17 14:00:16 +0300

    Changes

--- src/com/github/aroq/workflowlibs/actions/Druflow.groovy
+++ src/com/github/aroq/workflowlibs/actions/Druflow.groovy
@@ -1,15 +1,18 @@
 package com.github.aroq.workflowlibs.actions
 
 def deployFlow(params) {
-    debugLog(params, params, 'Deploy Flow')
+    timeout(time: 1, unit: 'MINUTES') {
+      input 'Deploy to Production?'
+        debugLog(params, params, 'Deploy Flow')
 
-    options = ''
-    if (fileExists(file: params.propertiesFile)) {
-        options = getOptions(readProperties(file: params.propertiesFile))
-    }
-    dir('druflow') {
-        git 'https://github.com/aroq/druflow.git'
-        sh "./gradlew app -Ddebug=${debug} -DprojectName=${projectName} -Denv=${environment} -DexecuteCommand=${executeCommand} -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir}${options}"
+        options = ''
+        if (fileExists(file: params.propertiesFile)) {
+            options = getOptions(readProperties(file: params.propertiesFile))
+        }
+        dir('druflow') {
+            git 'https://github.com/aroq/druflow.git'
+            sh "./gradlew app -Ddebug=${debug} -DprojectName=${projectName} -Denv=${environment} -DexecuteCommand=${executeCommand} -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir}${options}"
+        }
     }
 }
 
--- vars/pipelineTriggerDocman.groovy
+++ vars/pipelineTriggerDocman.groovy
@@ -33,21 +33,23 @@ def call(body) {
                     buildVersionType = 'tag'
                     break
             }
+            if (buildJob) {
+                build job: buildJob, parameters: [
+                    string(name: 'executeCommand', value: 'deployFlow'),
+                    string(name: 'projectName', value: env.gitlabSourceRepoName),
+                    string(name: 'environment', value: buildEnvironment),
+                    string(name: 'debug', value: '0'),
+                    string(name: 'simulate', value: '0'),
+                    string(name: 'docrootDir', value: 'docroot'),
+                    string(name: 'config_repo', value: params.configRepo),
+                    string(name: 'type', value: buildVersionType),
+                    string(name: 'version', value: env.gitlabSourceBranch),
+                    string(name: 'force', value: '0'),
+                    string(name: 'skip_stage_build', value: '0'),
+                    string(name: 'skip_stage_operations', value: '0'),
+                    string(name: 'skip_stage_test', value: '0')]
+            }
 
-            build job: buildJob, parameters: [
-                string(name: 'executeCommand', value: 'deployFlow'),
-                string(name: 'projectName', value: env.gitlabSourceRepoName),
-                string(name: 'environment', value: buildEnvironment),
-                string(name: 'debug', value: '0'),
-                string(name: 'simulate', value: '0'),
-                string(name: 'docrootDir', value: 'docroot'),
-                string(name: 'config_repo', value: params.configRepo),
-                string(name: 'type', value: buildVersionType),
-                string(name: 'version', value: env.gitlabSourceBranch),
-                string(name: 'force', value: '0'),
-                string(name: 'skip_stage_build', value: '0'),
-                string(name: 'skip_stage_operations', value: '0'),
-                string(name: 'skip_stage_test', value: '0')]
         }
     }
 }
