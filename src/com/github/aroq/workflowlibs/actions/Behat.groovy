5d6668d3856f2c2c08b1969d9e755ce9c6258745
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-18 23:28:40 +0300

    Changes

--- src/com/github/aroq/workflowlibs/actions/Behat.groovy
+++ src/com/github/aroq/workflowlibs/actions/Behat.groovy
@@ -1,22 +1,23 @@
 package com.github.aroq.workflowlibs.actions
 
 def perform(params) {
+    if (params.testEnvironment) {
+        testEnvironment = params.testEnvironment
+    }
+    else {
+        testEnvironment = environment
+    }
     dir('docroot/master/docroot') {
         if (fileExists('../bin/behat')) {
-//            try {
-                if (fileExists("../code/common/behat.${environment}.yml")) {
-                    sh """#!/bin/bash -l
+            if (fileExists("../code/common/behat.${testEnvironment}.yml")) {
+                sh """#!/bin/bash -l
 mkdir -p ../../../reports
-../bin/behat --config=../code/common/behat.${environment}.yml --format=pretty --out=std --format=junit --out=../../../reports
+../bin/behat --config=../code/common/behat.${testEnvironment}.yml --format=pretty --out=std --format=junit --out=../../../reports
 """
-                }
-                else {
-                    echo "Behat config file not found: ../code/common/behat.${environment}.yml"
-                }
-//            }
-//            catch (err) {
-//                currentBuild.result = 'UNSTABLE'
-//            }
+            }
+            else {
+                echo "Behat config file not found: ../code/common/behat.${testEnvironment}.yml"
+            }
         }
         else {
             echo "Behat execution file doesn't present"
