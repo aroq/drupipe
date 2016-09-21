63d7b925cb58df115fe4d40d8a672e0a098f07d7
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-21 18:40:05 +0300

    Changes

--- jobdsl/job_dsl_mothership.groovy
+++ jobdsl/job_dsl_mothership.groovy
@@ -17,7 +17,7 @@ projects.each {project ->
             cpsScm {
                 scm {
                     git(project.value['repo'])
-                    scriptPath(Jenkinsfile)
+                    scriptPath('Jenkinsfile')
                 }
             }
         }
