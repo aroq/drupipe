d12a172f5b8935a8eada1f693b7bb50783e9edfa
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-22 22:40:06 +0300

    Changes

--- vars/executePipeline.groovy
+++ vars/executePipeline.groovy
@@ -14,9 +14,9 @@ def call(body) {
     node {
         stages = [new com.github.aroq.workflowlibs.Stage(name: 'config', actionList: utils.processPipelineActionList([[action: 'Config.perform']]))]
 
-        _pipeline = utils.processPipeline(params.pipeline)
-        jsonDump(_pipeline, "Pipeline")
-        stages += utils.processPipeline(_pipeline)
+//        pipeline = utils.processPipeline(params.pipeline)
+//        jsonDump(pipeline, "Pipeline")
+        stages += utils.processPipeline(params.pipeline)
         stages += utils.processStages(params.stages)
 
         jsonDump(stages, "Pipeline stages")
