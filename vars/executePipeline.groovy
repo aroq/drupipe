c39be117abafbe901afb4c246ab984c3a33045f9
Alexander Tolstikov <atolstikov@adyax.com>
2016-09-22 19:35:27 +0300

    Changes

--- vars/executePipeline.groovy
+++ vars/executePipeline.groovy
@@ -12,13 +12,13 @@ def call(body) {
     utils = new com.github.aroq.workflowlibs.Utils()
 
     node {
-        stages = [new com.github.aroq.workflowlibs.Stage(name: 'config', actionList: utils.processPipelineActionList([[action: 'Config.perform']]))]
+        pipelineStages = [new com.github.aroq.workflowlibs.Stage(name: 'config', actionList: utils.processPipelineActionList([[action: 'Config.perform']]))]
 
         pipeline = utils.processPipeline(params.pipeline)
-        stages += pipeline
-        stages += utils.processStages(params.stages)
+        pipelineStages += pipeline
+        pipelineStages += utils.processStages(params.stages)
 
-        jsonDump(stages)
+        jsonDump(pipelineStages)
         if (jenkinsParam('force') == '1') {
             deleteDir()
         }
@@ -26,9 +26,9 @@ def call(body) {
             checkout scm
         }
 
-        for (int i = 0; i < stages.size(); i++) {
-            params.stage = stages[i]
-            params << executeStage(stages[i]) {
+        for (int i = 0; i < pipelineStages.size(); i++) {
+            params.stage = pipelineStages[i]
+            params << executeStage(pipelineStages[i]) {
                 p = params
             }
         }
