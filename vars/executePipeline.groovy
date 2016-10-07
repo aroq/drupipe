5cb75e337be9fa2b313b3fb51521c313e1d955df
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-07 20:27:21 +0300

    Added noNode param

--- vars/executePipeline.groovy
+++ vars/executePipeline.groovy
@@ -10,25 +10,35 @@ def call(body) {
     }
 
     utils = new com.github.aroq.workflowlibs.Utils()
-
-    node {
-        stages = [new com.github.aroq.workflowlibs.Stage(name: 'config', actionList: utils.processPipelineActionList([[action: 'Config.perform']]))]
-
-        stages += utils.processPipeline(params.pipeline)
-        stages += utils.processStages(params.stages)
-
-        if (jenkinsParam('force') == '1') {
-            deleteDir()
+    if (params.noNode) {
+        params << _executePipeline(params)
+    }
+    else {
+        node {
+            params << _executePipeline(params)
         }
-        if (params.checkoutSCM) {
-            checkout scm
-        }
+    }
 
-        for (int i = 0; i < stages.size(); i++) {
-            params.stage = stages[i]
-            params << executeStage(stages[i]) {
-                p = params
-            }
+    params
+}
+
+def _executePipeline(params) {
+    stages = [new com.github.aroq.workflowlibs.Stage(name: 'config', actionList: utils.processPipelineActionList([[action: 'Config.perform']]))]
+
+    stages += utils.processPipeline(params.pipeline)
+    stages += utils.processStages(params.stages)
+
+    if (jenkinsParam('force') == '1') {
+        deleteDir()
+    }
+    if (params.checkoutSCM) {
+        checkout scm
+    }
+
+    for (int i = 0; i < stages.size(); i++) {
+        params.stage = stages[i]
+        params << executeStage(stages[i]) {
+            p = params
         }
     }
     params
