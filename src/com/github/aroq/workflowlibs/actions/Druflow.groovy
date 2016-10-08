5fd87257d8dacba434b8d4c554744aa5eb5489ff
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-09 00:49:57 +0300

    Changed gradlew to gradle to test

--- src/com/github/aroq/workflowlibs/actions/Druflow.groovy
+++ src/com/github/aroq/workflowlibs/actions/Druflow.groovy
@@ -27,21 +27,24 @@ def deployFlow(params) {
     dir('druflow') {
         git 'https://github.com/aroq/druflow.git'
     }
-    sh "cd druflow && gradle app -Ddebug=${debug} -DprojectName=${deployProjectName} -Denv=${deployEnvironment} -DexecuteCommand=${executeCommand} -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir}${options}"
+    sh """#!/bin/bash -l
+    cd druflow && gradle app -Ddebug=${debug} -DprojectName=${deployProjectName} -Denv=${deployEnvironment} -DexecuteCommand=${executeCommand} -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir}${options}"""
 }
 
 def copySite(params) {
     dir('druflow') {
         git 'https://github.com/aroq/druflow.git'
     }
-    sh "cd druflow && gradle app -Ddebug=${debug} -Dsite=default -Denv=${params.fromEnvironment} -Dargument='${params.db} ${params.toEnvironment}' -DexecuteCommand=dbCopyAC -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir}"
+    sh """#!/bin/bash -l
+    cd druflow && gradle app -Ddebug=${debug} -Dsite=default -Denv=${params.fromEnvironment} -Dargument='${params.db} ${params.toEnvironment}' -DexecuteCommand=dbCopyAC -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir}"""
 }
 
 def dbBackupSite(params) {
     dir('druflow') {
         git 'https://github.com/aroq/druflow.git'
     }
-    sh "cd druflow && gradle app -Ddebug=${debug} -Dsite=default -Denv=${params.fromEnvironment} -Dargument=${params.db} -DexecuteCommand=dbBackupSite -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir}"
+    sh """#!/bin/bash -l
+    cd druflow && gradle app -Ddebug=${debug} -Dsite=default -Denv=${params.fromEnvironment} -Dargument=${params.db} -DexecuteCommand=dbBackupSite -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir}"""
 }
 
 @NonCPS
