3d441b5287a70c11aa5831441371ca105eac9dd4
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-08 20:14:49 +0300

    Changed seed job for docman type

--- src/com/github/aroq/workflowlibs/actions/Behat.groovy
+++ src/com/github/aroq/workflowlibs/actions/Behat.groovy
@@ -18,6 +18,8 @@ def perform(params) {
 
 //    dir('docroot/master/docroot') {
     sh('ls -al')
+    sh('ls docroot -al')
+    sh('ls docroot/bin -al')
     sh('ls docroot/bin/behat -al')
         if (fileExists('docroot/bin/behat')) {
             if (fileExists("docroot/code/common/behat.${testEnvironment}.yml")) {
