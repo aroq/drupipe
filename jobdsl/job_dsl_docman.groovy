f2aba040cf085d2fa60a215e976880223f05d167
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-14 00:26:16 +0300

    Added import for DocmanConfig on job dsl

--- jobdsl/job_dsl_docman.groovy
+++ jobdsl/job_dsl_docman.groovy
@@ -73,37 +73,37 @@ docmanConfig.states?.each { state ->
     }
 }
 
-pipelineJob("merge") {
-    concurrentBuild(false)
-    logRotator(-1, 30)
-    parameters {
-        stringParam('executeCommand', 'deployFlow')
-        stringParam('projectName', 'common')
-        stringParam('debug', '0')
-        stringParam('force', '0')
-        stringParam('simulate', '0')
-        stringParam('docrootDir', 'docroot')
-        stringParam('config_repo', config.configRepo)
-        stringParam('type', 'branch')
-        stringParam('version', 'state_stable')
-    }
-    definition {
-        cps {
-            script(mergePipeline)
-            sandbox()
-        }
-    }
-    triggers {
-        gitlabPush {
-            buildOnPushEvents(false)
-            buildOnMergeRequestEvents(true)
-            enableCiSkip()
-            useCiFeatures()
-        }
-    }
-    properties {
-        gitLabConnectionProperty {
-            gitLabConnection('Gitlab')
-        }
-    }
-}
+//pipelineJob("merge") {
+//    concurrentBuild(false)
+//    logRotator(-1, 30)
+//    parameters {
+//        stringParam('executeCommand', 'deployFlow')
+//        stringParam('projectName', 'common')
+//        stringParam('debug', '0')
+//        stringParam('force', '0')
+//        stringParam('simulate', '0')
+//        stringParam('docrootDir', 'docroot')
+//        stringParam('config_repo', config.configRepo)
+//        stringParam('type', 'branch')
+//        stringParam('version', 'state_stable')
+//    }
+//    definition {
+//        cps {
+//            script(mergePipeline)
+//            sandbox()
+//        }
+//    }
+//    triggers {
+//        gitlabPush {
+//            buildOnPushEvents(false)
+//            buildOnMergeRequestEvents(true)
+//            enableCiSkip()
+//            useCiFeatures()
+//        }
+//    }
+//    properties {
+//        gitLabConnectionProperty {
+//            gitLabConnection('Gitlab')
+//        }
+//    }
+//}
