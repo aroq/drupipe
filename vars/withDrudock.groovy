4081efcb76be35c8ce0bdce0e52eb660008d742a
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-13 23:04:16 +0300

    Added import for DocmanConfig on job dsl

---
+++ vars/withDrudock.groovy
@@ -0,0 +1,29 @@
+def call(body) {
+    def params = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = params
+    body()
+
+    if (params.p) {
+        params << params.p
+        params.remove('p')
+    }
+
+    node(params.nodeName) {
+        withCredentials([[$class: 'FileBinding', credentialsId: 'id_rsa', variable: 'ID_RSA_FILE']]) {
+            def drudock = docker.image('aroq/drudock:1.0.1')
+            drudock.pull()
+            drudock.inside('--user root:root') {
+                sshagent([params.gitCredentialsID]) {
+                    params = executePipeline {
+                        noNode = true
+                        gitCredentialsID = params.gitCredentialsID
+                        pipeline = params.pipeline
+                    }
+                }
+            }
+        }
+    }
+
+    params
+}
