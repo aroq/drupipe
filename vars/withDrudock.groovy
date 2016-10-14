f35572d50b7275180e3952551b8438bb369d4ef8
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-14 18:23:02 +0300

    Debug

--- vars/withDrudock.groovy
+++ vars/withDrudock.groovy
@@ -8,10 +8,13 @@ def call(params = [:], body) {
     echo "Credentials: ${params.credentialsID}"
 
     node(params.nodeName) {
+        echo "Credentials2: ${params.credentialsID}"
         withCredentials([[$class: 'FileBinding', credentialsId: 'id_rsa', variable: 'ID_RSA_FILE']]) {
+            echo "Credentials3: ${params.credentialsID}"
             def drudock = docker.image('aroq/drudock:1.0.1')
             drudock.pull()
             drudock.inside('--user root:root') {
+                echo "Credentials4: ${params.credentialsID}"
                 sshagent([params.credentialsID]) {
                     if (params.pipeline) {
                         params = executePipeline {
