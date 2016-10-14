4263be305aa1a4f9571ad3a64c0ea222a8f46fe0
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-14 09:51:33 +0300

    Added import for DocmanConfig on job dsl

--- src/com/github/aroq/workflowlibs/actions/Docman.groovy
+++ src/com/github/aroq/workflowlibs/actions/Docman.groovy
@@ -82,19 +82,19 @@ def info(params) {
     if (configRepo) {
         echo 'Docman init'
         sh('ls -al')
-//        sh(
-//            """#!/bin/bash -l
-//            docman init ${params.docrootDir} ${configRepo} -s
-//            """
-//        )
+        sh(
+            """#!/bin/bash -l
+            docman init ${params.docrootDir} ${configRepo} -s
+            """
+        )
     }
     echo 'Docman info'
-//    sh(
-//        """#!/bin/bash -l
-//        cd ${params.docrootDir}
-//        docman info full config.json
-//        """
-//    )
+    sh(
+        """#!/bin/bash -l
+        cd ${params.docrootDir}
+        docman info full config.json
+        """
+    )
 }
 
 def deploy(params) {
