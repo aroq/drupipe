71c98a2500392aadaa6c73b247f4143ad7d0bd00
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-17 21:53:04 +0300

    Debug

--- vars/executePipeline.groovy
+++ vars/executePipeline.groovy
@@ -4,26 +4,74 @@ def call(body) {
     body.delegate = params
     body()
 
-    if (params.p) {
-        params << params.p
-        params.remove('p')
-    }
+    try {
+        _pipelineNotify('STARTED')
+        if (params.p) {
+            params << params.p
+            params.remove('p')
+        }
 
-    if (!params.projectName) {
-        params.projectName = env.gitlabSourceRepoName
-    }
+        if (!params.projectName) {
+            params.projectName = env.gitlabSourceRepoName
+        }
 
-    utils = new com.github.aroq.workflowlibs.Utils()
-    if (params.noNode) {
-        params << _executePipeline(params)
-    }
-    else {
-        node {
+        utils = new com.github.aroq.workflowlibs.Utils()
+        if (params.noNode) {
             params << _executePipeline(params)
         }
+        else {
+            node {
+                params << _executePipeline(params)
+            }
+        }
+
+        params
+
+    }
+    catch (e) {
+        currentBuild.result = "FAILED"
+        throw e
+    }
+    finally {
+        _pipelineNotify(currentBuild.result)
     }
 
-    params
+}
+
+def _pipelineNotify(String buildStatus = 'STARTED') {
+    // build status of null means successful
+    buildStatus =  buildStatus ?: 'SUCCESSFUL'
+
+    // Default values
+    def colorName = 'RED'
+    def colorCode = '#FF0000'
+    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    def summary = "${subject} (${env.BUILD_URL})"
+    def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+
+    // Override default values based on build status
+    if (buildStatus == 'STARTED') {
+        color = 'YELLOW'
+        colorCode = '#FFFF00'
+    } else if (buildStatus == 'SUCCESSFUL') {
+        color = 'GREEN'
+        colorCode = '#00FF00'
+    } else {
+        color = 'RED'
+        colorCode = '#FF0000'
+    }
+
+    // Send notifications
+    slackSend (color: colorCode, message: summary, channel: '#faurecia')
+
+    // hipchatSend (color: color, notify: true, message: summary)
+
+    emailext (
+        subject: subject,
+        body: details,
+        recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+    )
 }
 
 def _executePipeline(params) {
@@ -47,4 +95,6 @@ def _executePipeline(params) {
     }
     params
 }
+
+
 
