fef1a83b60f61ed8181e221cec45fe6f9700b767
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-23 17:36:12 +0300

    debug

--- vars/withDrudock.groovy
+++ vars/withDrudock.groovy
@@ -6,18 +6,20 @@ def call(params = [:], body) {
     }
 
     node(params.nodeName) {
-        sshagent([params.credentialsID]) {
-        def drudock = docker.image('aroq/drudock:1.0.1')
-        drudock.pull()
-        drudock.inside('--user root:root') {
-                if (params.pipeline) {
-                    params = executePipeline {
-                        noNode = true
-                        credentialsID = params.credentialsID
-                        pipeline = params.pipeline
+        withCredentials([[$class: 'FileBinding', credentialsId: 'id_rsa', variable: 'ID_RSA_FILE']]) {
+            def drudock = docker.image('aroq/drudock:1.0.1')
+            drudock.pull()
+            drudock.inside('--user root:root') {
+                sshagent([params.credentialsID]) {
+                    if (params.pipeline) {
+                        params = executePipeline {
+                            noNode = true
+                            credentialsID = params.credentialsID
+                            pipeline = params.pipeline
+                        }
                     }
+                    body()
                 }
-                body()
             }
         }
     }
