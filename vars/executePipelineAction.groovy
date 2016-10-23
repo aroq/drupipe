e47a4461dba17d0b4343c5b518d0f3b676f29d64
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-24 01:06:53 +0300

    debug

--- vars/executePipelineAction.groovy
+++ vars/executePipelineAction.groovy
@@ -1,15 +1,5 @@
-def call(action, body) {
-    def params = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = params
-    body()
-
-    jsonDump(params.p, 'params.p')
-
-    if (params.p) {
-        params << params.p
-        params.remove('p')
-    }
+def call(action, params) {
+    jsonDump(params, 'params')
 
     utils = new com.github.aroq.workflowlibs.Utils()
     params << executeAction(utils.processPipelineAction(action)) {
