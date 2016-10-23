fa21768697086f299e43fdce8c2a40bfb4071b23
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-24 01:21:01 +0300

    debug

--- vars/executePipelineAction.groovy
+++ vars/executePipelineAction.groovy
@@ -1,6 +1,4 @@
 def call(action, params) {
-    jsonDump(params, 'params')
-
     utils = new com.github.aroq.workflowlibs.Utils()
     params << executeAction(utils.processPipelineAction(action)) {
         p = params
