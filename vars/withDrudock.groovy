50446f18e677ee82e6cc9c7119f5ec51b3d4ea41
Alexander Tolstikov <atolstikov@adyax.com>
2016-10-23 16:35:37 +0300

    debug

--- vars/withDrudock.groovy
+++ vars/withDrudock.groovy
@@ -6,35 +6,21 @@ def call(params = [:], body) {
     }
 
     node(params.nodeName) {
-//        withCredentials([[$class: 'FileBinding', credentialsId: 'id_rsa', variable: 'ID_RSA_FILE']]) {
-            def drudock = docker.image('aroq/drudock:1.0.1')
-            drudock.pull()
-            drudock.inside('--user root:root') {
-                echo "Credentials: ${params.credentialsID}"
-                sshagent([params.credentialsID]) {
-                    echo 'ssh'
-                    sh "ssh -v git@code.adyax.com"
-
-                    //echo 'git without credentials'
-                    //git url: 'git@code.adyax.com:CI-Sample-Multirepo/config.git', branch: 'master'
-
-                    echo 'git with credentials'
-                    git credentialsId: params.credentialsID, url: 'git@code.adyax.com:CI-Sample-Multirepo/config.git', branch: 'master'
-
-                    echo 'checkout scm'
-                    checkout scm
-                    if (params.pipeline) {
-                        params = executePipeline {
-                            noNode = true
-                            credentialsID = params.credentialsID
-                            pipeline = params.pipeline
-                        }
+        def drudock = docker.image('aroq/drudock:1.0.1')
+        drudock.pull()
+        drudock.inside('--user root:root') {
+            sshagent([params.credentialsID]) {
+                if (params.pipeline) {
+                    params = executePipeline {
+                        noNode = true
+                        credentialsID = params.credentialsID
+                        pipeline = params.pipeline
                     }
-                    body()
                 }
+                body()
             }
         }
-//    }
+    }
 
     params
 }
