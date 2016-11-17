3d8eee8052cada5b419b8945d603204e6f2e4a09
Alexander Tolstikov <atolstikov@adyax.com>
2016-11-17 20:14:32 +0300

    drupipe dockerfile added

--- vars/withDrupipeDocker.groovy
+++ vars/withDrupipeDocker.groovy
@@ -2,8 +2,12 @@ def call(commandParams = [:], body) {
     node(commandParams.nodeName) {
         configParams = executePipelineAction([action: 'Config.perform'], commandParams.clone() << params)
         commandParams << ((configParams << configParams.actionParams['withDrupipeDocker']) << commandParams)
-
-        def image = docker.image(commandParams.drupipeDockerImageName)
+        if (commandParams.dockerfile) {
+            def image = docker.build(commandParams.dockerfile, 'docroot/config')
+        }
+        else {
+            def image = docker.image(commandParams.drupipeDockerImageName)
+        }
         image.pull()
         image.inside(commandParams.drupipeDockerArgs) {
             sshagent([commandParams.credentialsID]) {
