65f1c8b7ffa8561ee6f3d0e75ce6f7c4a67a0dba
Alexander Tolstikov <atolstikov@adyax.com>
2016-12-04 14:10:15 +0300

    Refactoring druflow command

--- src/com/github/aroq/workflowlibs/actions/Druflow.groovy
+++ src/com/github/aroq/workflowlibs/actions/Druflow.groovy
@@ -64,7 +64,7 @@ def executeDruflowCommand(params, argument = '', overrides = [:]) {
 }
 
 def copySite(params) {
-    executeDruflowCommand(params, "${params.db} ${params.toEnvironment}, [env: params.executeEnvironment, site: 'default'])
+    executeDruflowCommand(params, "${params.db} ${params.toEnvironment}", [env: params.executeEnvironment, site: 'default'])
     // druflowGet(params)
 
     // sh "cd druflow && ./gradlew app -Ddebug=${debugFlag()} -Dsite=default -Denv=${params.executeEnvironment} -Dargument='${params.db} ${params.toEnvironment}' -DexecuteCommand=dbCopyAC -Dworkspace=${params.workspace} -DdocrootDir=${docrootDir}"
