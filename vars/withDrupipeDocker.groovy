0fe1334490a86bfec346ec97d73169a45bf53745
Alexander Tolstikov <atolstikov@adyax.com>
2016-12-30 22:43:38 +0500

    Added timestamps

--- vars/withDrupipeDocker.groovy
+++ vars/withDrupipeDocker.groovy
@@ -1,19 +1,21 @@
 def call(commandParams = [:], body) {
     node(commandParams.nodeName) {
-        configParams = executePipelineAction([action: 'Config.perform'], commandParams.clone() << params)
-        commandParams << ((configParams << configParams.actionParams['withDrupipeDocker']) << commandParams)
-        if (commandParams.dockerfile) {
-            image = docker.build(commandParams.dockerfile, 'docroot/config')
-        }
-        else {
-            image = docker.image(commandParams.drupipeDockerImageName)
-            image.pull()
-        }
-        image.inside(commandParams.drupipeDockerArgs) {
-            sshagent([commandParams.credentialsID]) {
-                result = body(commandParams)
-                if (result) {
-                    commandParams << result
+        timestamper {
+            configParams = executePipelineAction([action: 'Config.perform'], commandParams.clone() << params)
+            commandParams << ((configParams << configParams.actionParams['withDrupipeDocker']) << commandParams)
+            if (commandParams.dockerfile) {
+                image = docker.build(commandParams.dockerfile, 'docroot/config')
+            }
+            else {
+                image = docker.image(commandParams.drupipeDockerImageName)
+                image.pull()
+            }
+            image.inside(commandParams.drupipeDockerArgs) {
+                sshagent([commandParams.credentialsID]) {
+                    result = body(commandParams)
+                    if (result) {
+                        commandParams << result
+                    }
                 }
             }
         }
