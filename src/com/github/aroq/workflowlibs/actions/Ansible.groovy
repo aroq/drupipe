4cfb95e01be482e74aa8f61b240a44e2a33d089d
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-01 14:21:37 +0500

    Added Ansible action

---
+++ src/com/github/aroq/workflowlibs/actions/Ansible.groovy
@@ -0,0 +1,27 @@
+package com.github.aroq.workflowlibs.actions
+
+def deployWithGit(params) {
+    params.ansible = [
+        playbook: 'docroot/config/ansible/delivery.yml',
+        hostsFile: 'docroot/config/ansible/inventory.ini',
+        target: 'demo',
+        user: 'zebra',
+        repo: 'aroq@svn-2625.devcloud.hosting.acquia.com:aroq.git',
+        reference: 'develop',
+        deploy_to: '/var/www/dev'
+    ]
+    executeAnsiblePlaybook(params)
+}
+
+def executeAnsiblePlaybook(params) {
+sh"""#!/bin/bash -l
+ansible-playbook ${params.ansible.playbook} \
+-i ${params.ansible.hostsFile} \
+-e 'target=${params.ansible.target} \
+user=${params.ansible.user} \
+repo=${params.ansible.repo} \
+reference=${params.ansible.repo} \
+deploy_to=${params.ansible.deploy_to}'
+"""
+}
+
