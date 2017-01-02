ed8a90cc704cd5ac46887286c4b068eca22651c1
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-02 21:33:58 +0500

    Removed timeout

--- vars/withDrupipeDocker.groovy
+++ vars/withDrupipeDocker.groovy
@@ -1,22 +1,20 @@
 def call(commandParams = [:], body) {
     node(commandParams.nodeName) {
         timestamps {
-            timeout(20) {
-                configParams = executePipelineAction([action: 'Config.perform'], commandParams.clone() << params)
-                commandParams << ((configParams << configParams.actionParams['withDrupipeDocker']) << commandParams)
-                if (commandParams.dockerfile) {
-                    image = docker.build(commandParams.dockerfile, 'docroot/config')
-                }
-                else {
-                    image = docker.image(commandParams.drupipeDockerImageName)
-                    image.pull()
-                }
-                image.inside(commandParams.drupipeDockerArgs) {
-                    sshagent([commandParams.credentialsID]) {
-                        result = body(commandParams)
-                        if (result) {
-                            commandParams << result
-                        }
+            configParams = executePipelineAction([action: 'Config.perform'], commandParams.clone() << params)
+            commandParams << ((configParams << configParams.actionParams['withDrupipeDocker']) << commandParams)
+            if (commandParams.dockerfile) {
+                image = docker.build(commandParams.dockerfile, 'docroot/config')
+            }
+            else {
+                image = docker.image(commandParams.drupipeDockerImageName)
+                image.pull()
+            }
+            image.inside(commandParams.drupipeDockerArgs) {
+                sshagent([commandParams.credentialsID]) {
+                    result = body(commandParams)
+                    if (result) {
+                        commandParams << result
                     }
                 }
             }
