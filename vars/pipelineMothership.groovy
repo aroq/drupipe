79f665ea77eb67f92e314e76b76988e317e1f32d
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-02 16:12:17 +0500

    Changed mothership pipeline

--- vars/pipelineMothership.groovy
+++ vars/pipelineMothership.groovy
@@ -5,22 +5,23 @@ def call(body) {
     body()
 
     node('master') {
-        config = executePipeline {
-            noNode = true
-            pipeline =
-                [
-                    'seed': [
-                        [
-                            action: 'JobDslSeed.perform',
-                            params: [
-                                lookupStrategy: 'JENKINS_ROOT',
-                                jobsPattern: ['library/jobdsl/job_dsl_mothership.groovy'],
-                            ]
-                        ],
+        def drupipePipeline =
+            [
+                'seed': [
+                    [
+                        action: 'JobDslSeed.perform',
+                        params: [
+                            lookupStrategy: 'JENKINS_ROOT',
+                            jobsPattern: ['library/jobdsl/job_dsl_mothership.groovy'],
+                        ]
                     ],
-                ]
+                ],
+            ]
+
+        executeStages([
+            pipeline: drupipePipeline
             p = params
-        }
+        ])
     }
 }
 
