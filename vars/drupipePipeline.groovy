148db38c91482348242f7ffbc935480da22bad8c
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-03 22:48:17 +0500

    debug

--- vars/drupipePipeline.groovy
+++ vars/drupipePipeline.groovy
@@ -3,6 +3,7 @@
 def call(pipe, body = null) {
     drupipe() { config ->
         node(config.nodeName) {
+            echo "CONFIG PARAMS:${config}"
             withDrupipeDocker(config) {
                 drupipeStages(pipe.stages, config)
             }
