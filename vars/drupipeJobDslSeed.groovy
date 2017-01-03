ccdf679835c5e6ce31833eabae12847a6336b81e
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-03 16:28:46 +0500

    Refactoring params

--- vars/drupipeJobDslSeed.groovy
+++ vars/drupipeJobDslSeed.groovy
@@ -19,10 +19,11 @@ def call(body) {
                 // it - commandParams from body(commandParams)
                 params << it
                 checkout scm
-                params << executePipelineAction(action: 'Docman.info', params)
+                parameters = executePipelineAction(action: 'Docman.info', params)
 
                 stash name: 'config', includes: 'docroot/config/**, library/**, mothership/**', excludes: '.git, .git/**'
 
+                parameters
             }
         }
 
@@ -43,7 +44,7 @@ def call(body) {
             if (fileExists('docroot/config/pipelines/jobdsl')) {
                 parameters.actionParams.JobDslSeed_perform.jobsPattern << 'docroot/config/pipelines/jobdsl/*.groovy'
             }
-            executePipelineAction(action: 'JobDslSeed.perform', params)
+            executePipelineAction(action: 'JobDslSeed.perform', parameters)
         }
     }
 
