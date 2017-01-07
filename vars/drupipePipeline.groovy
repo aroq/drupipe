114ca7dbeb77ebf722cae65cfa1500bd9b127fa9
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-07 14:29:12 +0500

    Refactoring

--- vars/drupipePipeline.groovy
+++ vars/drupipePipeline.groovy
@@ -1,6 +1,9 @@
 #!groovy
 
 def call(pipe, body = null) {
+    if (!pipe.params) {
+        pipe.params = [:]
+    }
     drupipe(pipe.params) { config ->
         node(config.nodeName) {
             if (config.drupipeDocker) {
