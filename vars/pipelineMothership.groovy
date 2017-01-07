c8d4c8d6f24bfe3876c5c79c3533c33a07a03b52
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-07 14:36:06 +0500

    Refactoring

--- vars/pipelineMothership.groovy
+++ vars/pipelineMothership.groovy
@@ -27,10 +27,12 @@ def call(body) {
         stages:
             [
                 seed: [
-                    action: 'JobDslSeed.perform',
-                    params: [
-                        lookupStrategy: 'JENKINS_ROOT',
-                        jobsPattern: ['library/jobdsl/job_dsl_mothership.groovy'],
+                    [
+                        action: 'JobDslSeed.perform',
+                        params: [
+                            lookupStrategy: 'JENKINS_ROOT',
+                            jobsPattern: ['library/jobdsl/job_dsl_mothership.groovy'],
+                        ],
                     ]
                 ],
             ],
