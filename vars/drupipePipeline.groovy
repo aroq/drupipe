80bea5ab6c3d4b2b4886659da61422fc41034f2f
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-09 21:14:50 +0500

    Steps without node execution

--- vars/drupipePipeline.groovy
+++ vars/drupipePipeline.groovy
@@ -5,15 +5,20 @@ def call(pipe, body = null) {
         pipe.params = [:]
     }
     drupipe(pipe.params) { config ->
-        node(config.nodeName) {
-            if (config.drupipeDocker) {
-                withDrupipeDocker(config) {
+        if (config.nodeName) {
+            node(config.nodeName) {
+                if (config.drupipeDocker) {
+                    withDrupipeDocker(config) {
+                        drupipeStages(pipe.stages, config)
+                    }
+                }
+                else {
                     drupipeStages(pipe.stages, config)
                 }
             }
-            else {
-                drupipeStages(pipe.stages, config)
-            }
+        }
+        else {
+            drupipeStages(pipe.stages, config)
         }
     }
 }
