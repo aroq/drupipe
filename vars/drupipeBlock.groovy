81db64959ba8b3df7b2c1a02bd3056a2227f2682
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-10 11:25:59 +0500

    debug

--- vars/drupipeBlock.groovy
+++ vars/drupipeBlock.groovy
@@ -2,26 +2,27 @@
 
 def call(blockParams = [:], config, body) {
     def result = [:]
+    config.block = [:]
     echo "BLOCK PARAMS: ${blockParams}"
     echo "BLOCK CONFIG: ${config}"
     if (blockParams.nodeName) {
-        config.nodeName = blockParams.nodeName
+        config.block.nodeName = blockParams.nodeName
         node(config.nodeName) {
             if (blockParams.drupipeDocker) {
-                config.drupipeDocker = blockParams.drupipeDocker
+                config.block.drupipeDocker = blockParams.drupipeDocker
                 withDrupipeDocker(config) {
                     result = body()
                 }
             }
             else {
-                config.drupipeDocker = null
+                config.block.drupipeDocker = null
                 result = body()
             }
         }
     }
     else {
-        config.nodeName = null
-        config.drupipeDocker = null
+        config.block.nodeName = null
+        config.block.drupipeDocker = null
         result = body()
     }
     result
