2b5c389192aca3e910b7dcb529eb2bdc3dbb9881
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-11 15:28:26 +0500

    Refactoring

--- vars/drupipeStage.groovy
+++ vars/drupipeStage.groovy
@@ -2,7 +2,7 @@ import com.github.aroq.drupipe.Stage
 
 def call(name, config, body) {
 //    utils = new com.github.aroq.drupipe.Utils()
-    new Stage(name: name, params: config).execute(body)
+    new Stage(name: name, params: config, script: this).execute(body)
 //    utils._executeStage(name, config, body)
 }
 
