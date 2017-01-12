e40f857094f5a1e6ee41663d9215df82ae774253
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-13 00:11:27 +0500

    Refactoring

--- vars/drupipeAction.groovy
+++ vars/drupipeAction.groovy
@@ -1,5 +1,4 @@
 def call(action, context = [:]) {
-    utils = new com.github.aroq.drupipe.Utils()
-    context << context.pipeline.processPipelineAction(action, context).execute(context)
+    context << (context.pipeline.processPipelineAction(action, context)).execute(context)
 }
 
