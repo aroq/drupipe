8e908069a92f50d560da8aa25db27a001757fb87
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-12 23:47:25 +0500

    Refactoring

--- vars/pipelineMothership.groovy
+++ vars/pipelineMothership.groovy
@@ -1,17 +1,15 @@
 def call() {
     drupipe { context ->
         drupipeBlock(nodeName: 'master', context) {
-            drupipeStages([
-                seed: [
-                    [
-                        action: 'JobDslSeed.perform',
-                        params: [
-                            lookupStrategy: 'JENKINS_ROOT',
-                            jobsPattern: ['library/jobdsl/job_dsl_mothership.groovy'],
-                        ],
+            drupipeStage('seed', context) {
+                drupipeAction(
+                    action: 'JobDslSeed.perform',
+                    params: [
+                        lookupStrategy: 'JENKINS_ROOT',
+                        jobsPattern: ['library/jobdsl/job_dsl_mothership.groovy']
                     ],
-                ]
-            ], context)
+                context)
+            }
         }
     }
 }
