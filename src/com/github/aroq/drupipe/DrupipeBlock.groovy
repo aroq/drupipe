7adddfa4a7b9c8deba5a9fe745aad04769f0905c
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-13 16:29:52 +0500

    Refactoring

--- src/com/github/aroq/drupipe/DrupipeBlock.groovy
+++ src/com/github/aroq/drupipe/DrupipeBlock.groovy
@@ -9,56 +9,6 @@ class DrupipeBlock implements Serializable {
     LinkedHashMap context = [:]
 
     def execute(context, body = null) {
-        context.pipeline.script.echo "blockParams: ${blockParams}"
-        if (context) {
-            this.context = context
-        }
-
-        def result = [:]
-        context.block = [:]
-        context.pipeline.script.echo "CONTEXT: ${context}"
-
-//        if (blockParams.nodeName) {
-//            context.block.nodeName = blockParams.nodeName
-//            context.pipeline.node(context.block.nodeName) {
-//                if (blockParams.drupipeDocker) {
-//                    context.block.drupipeDocker = blockParams.drupipeDocker
-//                    context.pipeline.drupipeWithDocker(context) {
-//                        result = body()
-//                    }
-//                }
-//                else {
-//                    result = body()
-//                }
-//            }
-//        }
-//        else {
-//            result = body()
-//        }
-
-        if (blockParams.nodeName) {
-            context.block.nodeName = blockParams.nodeName
-            context.pipeline.script.node(context.nodeName) {
-                context.block.nodeName = blockParams.nodeName
-                if (context.drupipeDocker) {
-                    context.block.drupipeDocker = blockParams.drupipeDocker
-                    context.pipeline.script.drupipeWithDocker(context) {
-                        result = _execute(body)
-                    }
-                }
-                else {
-                    context.block.drupipeDocker = null
-                    result = _execute(body)
-                }
-            }
-        }
-        else {
-            context.block.nodeName = null
-            context.block.drupipeDocker = null
-            result = _execute(body)
-        }
-
-        result
     }
 
     def _execute(body = null) {
