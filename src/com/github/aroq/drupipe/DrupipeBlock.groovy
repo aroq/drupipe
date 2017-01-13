7b0929b618684908c0ffc38bd4a504e8feb9cd9a
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-13 16:42:13 +0500

    Refactoring

--- src/com/github/aroq/drupipe/DrupipeBlock.groovy
+++ src/com/github/aroq/drupipe/DrupipeBlock.groovy
@@ -4,11 +4,43 @@ class DrupipeBlock implements Serializable {
 
     ArrayList<DrupipeStage> stages = []
 
-    LinkedHashMap blockParams = [:]
+    String nodeName = ''
+
+    Boolean drupipeDocker = false
 
     LinkedHashMap context = [:]
 
     def execute(context, body = null) {
+        if (context) {
+            this.context = context
+        }
+
+        def result = [:]
+        context.block = [:]
+
+        if (this.nodeName) {
+            context.block.nodeName = this.nodeName
+            context.pipeline.script.node(context.nodeName) {
+                context.block.nodeName = this.nodeName
+                if (context.drupipeDocker) {
+                    context.block.drupipeDocker = this.drupipeDocker
+                    context.pipeline.script.drupipeWithDocker(context) {
+                        result = _execute(body)
+                    }
+                }
+                else {
+                    context.block.drupipeDocker = null
+                    result = _execute(body)
+                }
+            }
+        }
+        else {
+            context.block.nodeName = null
+            context.block.drupipeDocker = null
+            result = _execute(body)
+        }
+
+        result
     }
 
     def _execute(body = null) {
