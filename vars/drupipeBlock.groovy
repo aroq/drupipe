f58bca402b42547f32465afad4786f66a93805b0
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-13 16:21:22 +0500

    Refactoring

--- vars/drupipeBlock.groovy
+++ vars/drupipeBlock.groovy
@@ -5,6 +5,5 @@ import com.github.aroq.drupipe.DrupipeBlock
 def call(blockParams = [:], config, body) {
     echo "blockParams: ${blockParams}"
     echo "config: ${config}"
-    echo "body: ${body}"
     (new DrupipeBlock(blockParams)).execute(config, body)
 }
