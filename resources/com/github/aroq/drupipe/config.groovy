216c06daba5b47206b505c77e079aef092052db7
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-18 16:54:13 +0300

    Refactoring

--- resources/com/github/aroq/drupipe/config.groovy
+++ resources/com/github/aroq/drupipe/config.groovy
@@ -25,7 +25,7 @@ environments {
     prod {
         drupipeLibraryBranch = 'v0.3.2'
         drupipeLibraryType = 'tag'
-        dockerImage = 'aroq/drudock:1.3.0'
+        dockerImage = 'aroq/drudock:dev'
     }
 }
 
