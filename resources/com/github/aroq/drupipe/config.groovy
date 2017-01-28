a305f695ab58ea6f42909a576963bdf24c4b0689
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-28 19:16:49 +0300

    Changed drupipe version to v0.3.8

--- resources/com/github/aroq/drupipe/config.groovy
+++ resources/com/github/aroq/drupipe/config.groovy
@@ -21,7 +21,7 @@ environments {
         dockerImage = 'aroq/drudock:latest'
     }
     prod {
-        drupipeLibraryBranch = 'v0.3.7'
+        drupipeLibraryBranch = 'v0.3.8'
         drupipeLibraryType = 'tag'
         dockerImage = 'aroq/drudock:1.3.1'
     }
