7ed2d7c265a9c5ce2d48033829152dea1ed7a614
Alexander Tolstikov <atolstikov@adyax.com>
2017-01-29 12:44:37 +0300

    Refactoring

--- vars/drupipeYamlPipeline.groovy
+++ vars/drupipeYamlPipeline.groovy
@@ -17,7 +17,7 @@ def call(yamlFileName = null) {
 
 @NonCPS
 def drupipeGetPipeline(yamlFile) {
-    Yaml yaml = new Yaml(new CustomClassLoaderConstructor(getClass().getClassLoader()));
+    Yaml yaml = new Yaml(new org.yaml.snakeyaml.CustomClassLoaderConstructor(getClass().getClassLoader()));
     echo "yamlFile: ${yamlFile}"
     drupipePipeline = yaml.loadAs(yamlFile, DrupipePipeline.class);
     drupipePipeline.script = this
