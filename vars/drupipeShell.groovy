15283c397a5583cd8b8f71e53401aa92b7fef044
Alexander Tolstikov <atolstikov@adyax.com>
2017-02-05 15:13:16 +0300

    Added drupipeShell command

--- vars/drupipeShell.groovy
+++ vars/drupipeShell.groovy
@@ -2,6 +2,7 @@
 
 def call(shellCommand, context) {
     if (context.withKubernetes) {
+        echo "Executing ssh with SSH_AUTH_SOCK manually set"
         sh("SSH_AUTH_SOCK=${env.SSH_AUTH_SOCK} ${shellCommand}")
     }
     else {
