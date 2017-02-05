6bee46cb345d605a692b3edce3ee8cf8b2a18be1
Alexander Tolstikov <atolstikov@adyax.com>
2017-02-05 20:45:21 +0300

    Changed shell command for k8s

--- vars/drupipeShell.groovy
+++ vars/drupipeShell.groovy
@@ -5,7 +5,7 @@ def call(shellCommand, context) {
         echo "Executing ssh with SSH_AUTH_SOCK manually set"
         if (context.shellCommandWithBashLogin) {
             sh("""#!/bin/bash -l
-               export SSH_AUTH_SOCK=${env.SSH_AUTH_SOCK}
+               export SSH_AUTH_SOCK=${env.SSH_AUTH_SOCK};
                ${shellCommand}
                """)
         }
