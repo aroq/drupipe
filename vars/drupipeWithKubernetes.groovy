9efb1433104dfab89a7e59b38a1d9c7efcfcdd62
Alexander Tolstikov <atolstikov@adyax.com>
2017-02-06 12:01:01 +0300

    Added GitConfig action

--- vars/drupipeWithKubernetes.groovy
+++ vars/drupipeWithKubernetes.groovy
@@ -1,9 +1,9 @@
 def call(context = [:], body) {
     context << context.actionParams['drupipeWithKubernetes'] << context
     container(context.containerName) {
-        drupipeAction(action: 'GitConfig.set', context)
         context.workspace = pwd()
         sshagent([context.credentialsID]) {
+            drupipeAction(action: 'GitConfig.set', context)
             result = body(context)
             if (result) {
                 context << result
