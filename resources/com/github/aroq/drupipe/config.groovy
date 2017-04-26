b1c38021480f08df13a1c3002910033c229ff956
Alexander Tolstikov <atolstikov@adyax.com>
2017-04-26 11:21:38 +0300

    Changed ansible params

--- resources/com/github/aroq/drupipe/config.groovy
+++ resources/com/github/aroq/drupipe/config.groovy
@@ -44,7 +44,7 @@ defaultActionParams = [
         behat_args: '--format=pretty --out=std --format=junit',
     ],
     drupipeWithDocker: [
-        drupipeDockerArgs: '--user root:root',
+        drupipeDockerArgs: '--user root:root --net=host',
     ],
     drupipeWithKubernetes: [
         containerName: 'drudock',
