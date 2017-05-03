78196a4c484e96569193a87477ee2b91af660250
Alexander Tolstikov <atolstikov@adyax.com>
2017-05-03 16:44:59 +0300

    debug

--- src/com/github/aroq/drupipe/actions/YamlDeployer.groovy
+++ src/com/github/aroq/drupipe/actions/YamlDeployer.groovy
@@ -17,10 +17,33 @@ class YamlDeployer extends BaseAction {
         if (script.fileExists(deployFile)) {
             def deployYAML = script.readYaml(file: deployFile)
             utils.dump(deployYAML, 'DEPLOY YAML')
+            def commands = []
+            if (deployYaml.deploy) {
+                for (def i=0; i < deployYAML.deploy.size(); i++) {
+                    commands << setVariables(deployYaml.deploy[i])
+                }
+            }
+            if (commands) {
+                def joinedCommands = commands.join("\n")
+                executeCommand(joinedCommands)
+            }
         }
         else {
             script.echo "Deploy file ${deployFile} doesn't exist"
         }
+    }
+
+    def setVariables(String command) {
+        command
+    }
+
+    def executeCommand(String command) {
+        script.drupipeShell(
+            """
+            ${command}
+            """, context << [shellCommandWithBashLogin: true]
+        )
+
     }
 }
 
