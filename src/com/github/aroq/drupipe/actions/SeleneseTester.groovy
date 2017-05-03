80643a14333ba44e7d318af38c012af6a530ec6d
Alexander Tolstikov <atolstikov@adyax.com>
2017-05-03 22:37:16 +0300

    Selenese runner

--- src/com/github/aroq/drupipe/actions/SeleneseTester.groovy
+++ src/com/github/aroq/drupipe/actions/SeleneseTester.groovy
@@ -13,13 +13,20 @@ class SeleneseTester extends BaseAction {
     def DrupipeAction action
 
     def test() {
-        script.drupipeAction([action: "Git.clone", params: action.params], context)
+        sshagent([context.credentialsId]) {
+            script.drupipeAction([action: "Git.clone", params: action.params], context)
 
-        script.drupipeShell(
-        """
-        docker run --rm --user root:root -v "${context.workspace}:${context.workspace}" -e "SELENSE_BASE_URL=${action.params.SELENESE_BASE_URL}" -e "SCREEN_WIDTH=1600" -e "SCREEN_HEIGHT=1600" -e "SCREEN_DEPTH=24" --workdir "${context.workspace}/${action.params.dir}/${action.params.repoDirName}" --entrypoint "/opt/bin/entry_point.sh" ${action.params.dockerImage} "${action.params.suiteName}"
+            script.drupipeShell(
+                """
+        docker run --rm --user root:root -v "${context.workspace}:${context.workspace}" -e "SELENSE_BASE_URL=${
+                    action.params.SELENESE_BASE_URL
+                }" -e "SCREEN_WIDTH=1600" -e "SCREEN_HEIGHT=1600" -e "SCREEN_DEPTH=24" --workdir "${
+                    context.workspace
+                }/${action.params.dir}/${action.params.repoDirName}" --entrypoint "/opt/bin/entry_point.sh" ${
+                    action.params.dockerImage
+                } "${action.params.suiteName}"
         """, context)
-
+        }
     }
 
 }
