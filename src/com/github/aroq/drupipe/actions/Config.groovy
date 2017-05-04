b922ce3e489e9e4862278dca61aa8f9b523e7f74
Alexander Tolstikov <atolstikov@adyax.com>
2017-05-04 18:29:43 +0300

    Scenarios refactor

--- src/com/github/aroq/drupipe/actions/Config.groovy
+++ src/com/github/aroq/drupipe/actions/Config.groovy
@@ -124,7 +124,7 @@ class Config extends BaseAction {
                         scenarioSource = config.default_scenario_source
                         scenario.name = values[0]
                     }
-                    if (context.scenario_sources[scenarioSource]) {
+                    if (cnofig.scenario_sources[scenarioSource]) {
                         scenario.source = config.scenario_sources[scenarioSource]
                         if (!this.scenarioSources[scenario.source]) {
                             scenario.source.repoParams = [
