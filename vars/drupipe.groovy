a3ff487983167ff0da862e8ac9f15f35a3c43365
Alexander Tolstikov <atolstikov@adyax.com>
2017-05-04 23:05:17 +0300

    Scenarios refactor

--- vars/drupipe.groovy
+++ vars/drupipe.groovy
@@ -4,8 +4,8 @@ import com.github.aroq.drupipe.DrupipePipeline
 
 def call(context = [:], body) {
     // "params" is the globally available Jenkins params map (immutable).
-    sshagent(['zebra']) {
+    //sshagent(['zebra']) {
         context.pipeline = new DrupipePipeline(context: context, script: this, params: params)
         context.pipeline.execute(body)
-    }
+    //}
 }
