22712635c96a7e90d5684ba46e468cf840751a41
Alexander Tolstikov <atolstikov@adyax.com>
2017-05-11 17:09:22 +0300

    job dsl jobs

--- src/com/github/aroq/drupipe/actions/SeleneseTester.groovy
+++ src/com/github/aroq/drupipe/actions/SeleneseTester.groovy
@@ -32,7 +32,16 @@ class SeleneseTester extends BaseAction {
             }
         }
 
-        script.step([$class: 'SeleniumHtmlReportPublisher', testResultsDir: 'tests/selenese/reports-xml'])
+        script.publishHTML (target: [
+            allowMissing: false,
+            alwaysLinkToLastBuild: false,
+            keepAll: true,
+            reportDir: 'tests/selenese/reports',
+            reportFiles: 'index.html',
+            reportName: "Selenese report"
+        ])
+
+//        script.step([$class: 'SeleniumHtmlReportPublisher', testResultsDir: 'tests/selenese/reports-xml'])
 //        script.junit 'reports-xml/*.xml'
     }
 }
