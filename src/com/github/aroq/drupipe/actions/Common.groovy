4fb56fcbec71927f976dd37e869a05ce24ceb91f
Alexander Tolstikov <atolstikov@adyax.com>
2017-05-24 18:58:38 +0300

    Refactoring pipelines

--- src/com/github/aroq/drupipe/actions/Common.groovy
+++ src/com/github/aroq/drupipe/actions/Common.groovy
@@ -1,7 +1,21 @@
 package com.github.aroq.drupipe.actions
 
-def confirm(params) {
-    timeout(time: params.timeToConfirm, unit: 'MINUTES') {
-        input params.message
+import com.github.aroq.drupipe.DrupipeAction
+
+class Common extends BaseAction {
+
+    def context
+
+    def script
+
+    def utils
+
+    def DrupipeAction action
+
+    def confirm() {
+        def message = context.message
+        script.timeout(time: action.params.timeToConfirm, unit: 'MINUTES') {
+           this.script.input 'Do you want to proceed?'
+        }
     }
 }
