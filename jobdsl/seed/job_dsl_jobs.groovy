73afbcbe69fa3d1005df7e795ca4c4f4d0e78f69
Alexander Tolstikov <atolstikov@adyax.com>
2017-05-24 10:39:25 +0300

    Refactoring pipelines

--- jobdsl/seed/job_dsl_jobs.groovy
+++ jobdsl/seed/job_dsl_jobs.groovy
@@ -235,7 +235,8 @@ def processJob(jobs, currentFolder, config) {
             }
             else if (job.value.type == 'selenese') {
 //                def repo = config.defaultActionParams.SeleneseTester.repoAddress
-                println "Config: ${config.defaultActionParams}"
+                println "Config defaultActionParams: ${config.defaultActionParams}"
+                println "Config selenese tester: ${config.defaultActionParams}"
                 def b = config.defaultActionParams.SeleneseTester.reference ? config.defaultActionParams.SeleneseTester.reference : 'master'
 
                 if (config.env.GITLAB_API_TOKEN_TEXT) {
