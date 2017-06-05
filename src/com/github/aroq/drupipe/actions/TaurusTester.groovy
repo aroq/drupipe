486f97edfb7eaf6cf3cb84e50062932f838264ad
Alexander Tolstikov <atolstikov@adyax.com>
2017-06-05 18:37:00 +0300

    Added taurus command

--- src/com/github/aroq/drupipe/actions/TaurusTester.groovy
+++ src/com/github/aroq/drupipe/actions/TaurusTester.groovy
@@ -13,9 +13,7 @@ class TaurusTester extends BaseAction {
     def DrupipeAction action
 
     def test() {
-        def bztString = """${this.context.taurus_config} \
--frontpage-cached -o \
-execution.hold-for=${this.context.taurus_hold_for} \
+        def bztString = """${this.context.taurus_config} -o execution.hold-for=${this.context.taurus_hold_for} \
 -o execution.ramp-up=${this.context.taurus_ramp_up} -o \
 execution.concurrency=${this.context.taurus_concurrency} \
 ${this.context.taurus_args}"""
