130773d5fe503be6634037a3ac7c3da01c96a772
Alexander Tolstikov <atolstikov@adyax.com>
2017-06-05 14:09:45 +0300

    Added taurus command

---
+++ src/com/github/aroq/drupipe/actions/TaurusTester.groovy
@@ -0,0 +1,36 @@
+package com.github.aroq.drupipe.actions
+
+import com.github.aroq.drupipe.DrupipeAction
+
+class TaurusTester extends BaseAction {
+
+    def context
+
+    def script
+
+    def utils
+
+    def DrupipeAction action
+
+    def test() {
+//        def workspace = script.pwd()
+        def sourcePath = utils.sourcePath(context, action.params.sourceName, '')
+        try {
+            script.drupipeShell(
+
+"""cd ${sourcePath}; \
+${context.jenkinsParams.taurus_config} \
+-frontpage-cached -o \
+execution.hold-for=${context.jenkinsParams.taurus_hold_for} \
+-o execution.ramp-up=${context.jenkinsParams.taurus_ramp_up} -o \
+execution.concurrency=${context.jenkinsParams.taurus_concurrency} \
+${context.jenkinsParams.taurus_args}""",
+                context
+            )
+        }
+        catch (e) {
+            script.currentBuild.result = "UNSTABLE"
+        }
+    }
+}
+
