1cd420cee733b636d227efff8d796de07d5b264d
Alexander Tolstikov <atolstikov@adyax.com>
2017-08-31 22:02:07 +0300

    Changed dockerImage for k8s

--- vars/drupipeWithKubernetes.groovy
+++ vars/drupipeWithKubernetes.groovy
@@ -1,9 +1,9 @@
 def call(context = [:], body) {
     context << context.defaultActionParams['drupipeWithKubernetes'] << context
-    container(context.containerName) {
+    container(context.dockerImage) {
         context.workspace = pwd()
         sshagent([context.credentialsId]) {
-            drupipeShell("git config --global user.email 'drupipe@github.com'; git config --global user.name 'Drupipe'", context)
+//            drupipeShell("git config --global user.email 'drupipe@github.com'; git config --global user.name 'Drupipe'", context)
             result = body(context)
             if (result) {
                 context << result
