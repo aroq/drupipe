2568e4d62a723393a502fb8028a3514366f72adf
Alexander Tolstikov <atolstikov@adyax.com>
2017-09-01 11:45:21 +0300

    Changed dockerImage for k8s

--- vars/drupipeWithKubernetes.groovy
+++ vars/drupipeWithKubernetes.groovy
@@ -1,24 +1,13 @@
 def call(context = [:], body) {
+
     podTemplate(label: 'mypod', containers: [
         containerTemplate(name: 'docman', image: 'michaeltigr/zebra-build-php-drush-docman', ttyEnabled: true, command: 'cat'),
     ]) {
         node('mypod') {
             container('docman') {
+                unstash('config')
+                context << context.defaultActionParams['drupipeWithKubernetes'] << context
                 sh('drush --version')
-            }
-        }
-    }
-
-
-    nodeName = 'drupipe'
-    podTemplate(label: 'mypod', containers: [
-        containerTemplate(name: context.containerName, image: context.dockerImage, ttyEnabled: true, command: 'cat'),
-    ]) {
-        node('mypod') {
-            echo "NODE NAME: ${nodeName}"
-            unstash('config')
-            context << context.defaultActionParams['drupipeWithKubernetes'] << context
-            container(context.containerName) {
                 context.workspace = pwd()
                 drupipeShell("echo 'test'", context)
                 sshagent([context.credentialsId]) {
@@ -31,6 +20,17 @@ def call(context = [:], body) {
             }
         }
     }
+
+//    nodeName = 'drupipe'
+//    podTemplate(label: 'mypod', containers: [
+//        containerTemplate(name: context.containerName, image: context.dockerImage, ttyEnabled: true, command: 'cat'),
+//    ]) {
+//        node('mypod') {
+//            echo "NODE NAME: ${nodeName}"
+//            container(context.containerName) {
+//            }
+//        }
+//    }
 
 
     context
