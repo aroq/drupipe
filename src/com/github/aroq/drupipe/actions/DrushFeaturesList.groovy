2cfdfc3b4a5fdec959ffa8763dae4b75d479991c
Michael Molchanov <mmolchanov@adyax.com>
2017-09-06 17:55:08 +0300

    Add DrushFeaturesList action.

---
+++ src/com/github/aroq/drupipe/actions/DrushFeaturesList.groovy
@@ -0,0 +1,63 @@
+package com.github.aroq.drupipe.actions
+
+import groovy.json.JsonSlurperClassic
+import com.github.aroq.drupipe.DrupipeAction
+
+class DrushFeaturesList extends BaseAction {
+
+    def context
+
+    def script
+
+    def utils
+
+    String state = 'Overridden'
+
+    HashMap notification = [:]
+
+    def DrupipeAction action
+
+    def runCommand() {
+        def features = []
+
+        this.notification.level = "action:${this.context.drupipeStageName}"
+        this.notification.status = 'INFO'
+
+        this.context.drush_command = 'fl --format=json'
+        this.script.drupipeAction("Drush.runCommand", this.context)
+
+        def output = this.context.drupipeShellResult
+        def jsonOutput = new JsonSlurperClassic().parseText(output)
+        jsonOutput.each { key, feature->
+            if (feature.containsKey('state') && feature['state'] == this.state) {
+                features << feature
+            }
+        }
+
+        if (features.size() > 0) {
+            def exception_table = "|Name|Machine name|\n|:---|:---|\n"
+            features.each { feature ->
+                exception_table = exception_table + "|${feature['name']}|${feature['feature']}|"
+
+                this.context.drush_command = 'fd ' + feature['feature']
+                this.script.drupipeAction("Drush.runCommand", this.context)
+
+                def diff = this.context.drupipeShellResult
+                diff = diff.substring(diff.indexOf('Legend:'))
+
+                this.notification.message = '```' + diff + '```'
+
+                this.notification.name = "**${feature['name']}** (${feature['feature']})"
+
+                this.utils.pipelineNotify(this.context, this.notification)
+            }
+
+            throw new Exception("OVERRIDEN FEATURES:\n\n${exception_table}")
+        }
+        else {
+            this.script.echo "DRUSH FEATURES LIST: No overridden features."
+        }
+
+        features
+    }
+}
