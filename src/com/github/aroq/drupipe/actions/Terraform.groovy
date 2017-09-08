4ca42f93353090662239ffbe3cff8c3b0521e260
Alexander Tolstikov <atolstikov@adyax.com>
2017-09-08 17:52:53 +0300

    Added terraform action

---
+++ src/com/github/aroq/drupipe/actions/Terraform.groovy
@@ -0,0 +1,42 @@
+package com.github.aroq.drupipe.actions
+
+import com.github.aroq.drupipe.DrupipeAction
+
+class Terraform extends BaseAction {
+
+    def context
+
+    def script
+
+    def utils
+
+    def DrupipeAction action
+
+    String terraformExecutable = 'terraform'
+
+    def init() {
+        def sourceDir = utils.sourceDir(context, action.params.infraDir)
+        script.drupipeShell("""
+            cd ${sourceDir}
+            ${terraformExecutable} init -input=false
+            """, context)
+    }
+
+    def plan() {
+        def sourceDir = utils.sourceDir(context, action.params.infraDir)
+        script.withCredentials([string(credentialsId: 'DO_TOKEN', variable: 'DIGITALOCEAN_TOKEN')]) {
+            script.drupipeShell("""
+            cd ${sourceDir}
+            ${terraformExecutable} plan -auto-approve=true -input=false -state=terraform/dev/terraform.tfstate -var-file=terraform/dev/terraform.tfvars -var-file=terraform/dev/secrets.tfvars
+            """, context)
+        }
+    }
+
+    def apply() {
+
+    }
+
+    def destroy() {
+
+    }
+}
