c8cc1c7e6226e88f69a176c8b4d4dfff84fd55fb
Alexander Tolstikov <atolstikov@adyax.com>
2017-09-08 21:42:51 +0300

    Added withEnv to DrupipeAction

--- src/com/github/aroq/drupipe/DrupipeAction.groovy
+++ src/com/github/aroq/drupipe/DrupipeAction.groovy
@@ -53,36 +53,39 @@ class DrupipeAction implements Serializable {
             actionParams << this.params
             utils.debugLog(context, actionParams, "${this.fullName} action params")
 
-            // Execute action from file if exist in sources...
-            def actionFile = null
-            def actionResult = null
-            if (context.sourcesList) {
-                for (def i = 0; i < context.sourcesList.size(); i++) {
-                    def source = context.sourcesList[i]
-                    def fileName = utils.sourcePath(context, source.name, 'pipelines/actions/' + this.name + '.groovy')
-                    utils.debugLog(actionParams, fileName, "DrupipeAction file name to check")
-                    // To make sure we only check fileExists in Heavyweight executor mode.
-                    if (context.block?.nodeName && this.context.pipeline.script.fileExists(fileName)) {
-                        actionFile = this.context.pipeline.script.load(fileName)
-                        actionResult = actionFile."${this.methodName}"(actionParams)
+            def envParams = actionParams.env ? actionParams.env.collect{ k, v -> "$k=$v"} : []
+            withEnv(envParams) {
+                // Execute action from file if exist in sources...
+                def actionFile = null
+                def actionResult = null
+                if (context.sourcesList) {
+                    for (def i = 0; i < context.sourcesList.size(); i++) {
+                        def source = context.sourcesList[i]
+                        def fileName = utils.sourcePath(context, source.name, 'pipelines/actions/' + this.name + '.groovy')
+                        utils.debugLog(actionParams, fileName, "DrupipeAction file name to check")
+                        // To make sure we only check fileExists in Heavyweight executor mode.
+                        if (context.block?.nodeName && this.context.pipeline.script.fileExists(fileName)) {
+                            actionFile = this.context.pipeline.script.load(fileName)
+                            actionResult = actionFile."${this.methodName}"(actionParams)
+                        }
                     }
                 }
-            }
-            // ...Otherwise execute from class.
-            if (!actionFile) {
-                try {
-                    def actionInstance = this.class.classLoader.loadClass("com.github.aroq.drupipe.actions.${this.name}", true, false )?.newInstance(
-                        context: context,
-                        action: this,
-                        script: context.pipeline.script,
-                        utils: utils,
-                    )
+                // ...Otherwise execute from class.
+                if (!actionFile) {
+                    try {
+                        def actionInstance = this.class.classLoader.loadClass("com.github.aroq.drupipe.actions.${this.name}", true, false )?.newInstance(
+                            context: context,
+                            action: this,
+                            script: context.pipeline.script,
+                            utils: utils,
+                        )
 
-                    actionResult = actionInstance."${this.methodName}"()
-                }
-                catch (err) {
-                    this.context.pipeline.script.echo err.toString()
-                    throw err
+                        actionResult = actionInstance."${this.methodName}"()
+                    }
+                    catch (err) {
+                        this.context.pipeline.script.echo err.toString()
+                        throw err
+                    }
                 }
             }
 
