d294fee47fcb62afa817570e0a934ed6b436ed81
Alexander Tolstikov <atolstikov@adyax.com>
2017-09-09 16:22:51 +0300

    Store terraform state in file

--- src/com/github/aroq/drupipe/actions/Jenkins.groovy
+++ src/com/github/aroq/drupipe/actions/Jenkins.groovy
@@ -13,16 +13,11 @@ class Jenkins extends BaseAction {
     def DrupipeAction action
 
     def cli() {
-
         def inventory = script.readJSON file: 'terraform.inventory.json'
-
-        def sourceDir = utils.sourceDir(context, action.params.infraSourceName)
         this.script.withEnv(["JENKINS_URL=${inventory['zebra-jenkins-master'][0]}"]) {
             this.script.drupipeShell("""
-            cd ${sourceDir}
-            jenkins-cli-wrapper.sh help
+            /jenkins-cli/jenkins-cli-wrapper.sh help
             """, this.context)
-
         }
     }
 
