d2a2ac6b8646f554804d5d03377d94300f69aa86
Alexander Tolstikov <atolstikov@adyax.com>
2017-09-09 23:34:32 +0300

    Jenkins cli auth

--- src/com/github/aroq/drupipe/actions/Jenkins.groovy
+++ src/com/github/aroq/drupipe/actions/Jenkins.groovy
@@ -18,8 +18,8 @@ class Jenkins extends BaseAction {
         this.script.withCredentials(creds) {
             def envvars = ["JENKINS_URL=http://${inventory['zebra-jenkins-master'][0]}:${this.action.params.port}", 'PRIVATE_KEY=${ZEBRA_ID_RSA}']
             this.script.withEnv(envvars) {
+                this.script.drupipeShell('chmod 400 ${PRIVATE_KEY}', this.context)
                 this.script.drupipeShell("""
-                chmod 400 ${ZEBRA_ID_RSA}
                 /jenkins-cli/jenkins-cli-wrapper.sh help
                 """, this.context)
             }
