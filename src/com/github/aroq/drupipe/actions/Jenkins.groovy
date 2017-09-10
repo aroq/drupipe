4f4c4ce5013851785b272684d672e5e18e6491a5
Alexander Tolstikov <atolstikov@adyax.com>
2017-09-10 11:07:39 +0300

    Jenkins cli auth

--- src/com/github/aroq/drupipe/actions/Jenkins.groovy
+++ src/com/github/aroq/drupipe/actions/Jenkins.groovy
@@ -14,14 +14,13 @@ class Jenkins extends BaseAction {
 
     def cli() {
         def inventory = script.readJSON file: 'terraform.inventory.json'
-        def creds = [this.script.file(credentialsId: 'id_rsa', variable: 'PRIVATE_KEY')]
+        def creds = [this.script.string(credentialsId: 'JENKINS_API_TOKEN', variable: 'JENKINS_API_TOKEN')]
         this.script.withCredentials(creds) {
             def envvars = ["JENKINS_URL=http://${inventory['zebra-jenkins-master'][0]}:${this.action.params.port}"]
             this.script.withEnv(envvars) {
-                this.script.drupipeShell('chmod 400 ${PRIVATE_KEY}', this.context)
-                this.script.drupipeShell('''
-                /jenkins-cli/jenkins-cli-wrapper.sh -i ${PRIVATE_KEY} help
-                ''', this.context)
+                this.script.drupipeShell("""
+                /jenkins-cli/jenkins-cli-wrapper.sh -auth ${this.action.params.user}:\${JENKINS_API_TOKEN} help
+                """, this.context)
             }
         }
     }
