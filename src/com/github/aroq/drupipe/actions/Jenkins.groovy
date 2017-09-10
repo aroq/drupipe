5a63c480bff76602738c197c59d829555b909808
Alexander Tolstikov <atolstikov@adyax.com>
2017-09-10 14:48:57 +0300

    Jenkins seed test

--- src/com/github/aroq/drupipe/actions/Jenkins.groovy
+++ src/com/github/aroq/drupipe/actions/Jenkins.groovy
@@ -1,6 +1,7 @@
 package com.github.aroq.drupipe.actions
 
 import com.github.aroq.drupipe.DrupipeAction
+import groovy.json.JsonSlurperClassic
 
 class Jenkins extends BaseAction {
 
@@ -33,7 +34,7 @@ class Jenkins extends BaseAction {
     def seedTest() {
         def sourceDir = utils.sourceDir(context, 'mothership')
         def json = this.script.readFile("${sourceDir}/projects.json")
-        def projects = json.projects.find {k, v -> v.containsKey('tests') && v['tests'].contains('seed') }
+        def projects = JsonSlurperClassic.newInstance().parseText(json).projects.find {k, v -> v.containsKey('tests') && v['tests'].contains('seed') }
         projects.each {k, v ->
             this.action.params.jobName = "${k}/seed"
             build()
