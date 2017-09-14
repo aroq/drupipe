5c760fbdc534965f13655463b6bb03c2f84b424b
Alexander Tolstikov <atolstikov@adyax.com>
2017-09-14 23:41:31 +0300

    add parent config params for dsl jobs

--- jobdsl/seed/job_dsl_jobs.groovy
+++ jobdsl/seed/job_dsl_jobs.groovy
@@ -30,7 +30,7 @@ def processJob(jobs, currentFolder, config, parentConfigParams = [:]) {
         println "Type: ${job.value.type}"
         println "Current name: ${currentName}"
 
-        println "Job params: ${job.value.params}"
+        println "Job: ${job.value}"
         job.value.params = job.value.params ? job.value.params : [:]
         job.value.params << (parentConfigParams << job.value.params)
         println "Job params after parent params merge: ${job.value.params}"
