8b93373326820f9fec903a324f17ac11a6497d36
Alexander Tolstikov <atolstikov@adyax.com>
2017-09-17 18:31:08 +0300

    Fix JobDSL parent config params

--- jobdsl/seed/job_dsl_jobs.groovy
+++ jobdsl/seed/job_dsl_jobs.groovy
@@ -24,7 +24,8 @@ if (config.jobs) {
 def processJob(jobs, currentFolder, config, parentConfigParamsPassed = [:]) {
     def pipelineScript = config.pipeline_script ? config.pipeline_script : 'pipelines/pipeline'
     for (job in jobs) {
-        def parentConfigParams = parentConfigParamsPassed
+        def parentConfigParams
+        parentConfigParams << parentConfigParamsPassed
         println job
         println "Processing job: ${job.key}"
         def currentName = currentFolder ? "${currentFolder}/${job.key}" : job.key
