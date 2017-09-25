554be8411b01cc0807cc5e7426dc822d07eb1912
Alexander Tolstikov <atolstikov@adyax.com>
2017-09-25 11:06:05 +0200

    Setup k8s execution

---
+++ vars/drupipeExecuteBlocksWithKubernetes.groovy
@@ -0,0 +1,42 @@
+import com.github.aroq.drupipe.DrupipeBlock
+
+def call(context = [:], body) {
+    echo "Container mode: kubernetes"
+    def nodeName = 'drupipe'
+    def containers = []
+
+    for (def i = 0; i < blocks.size(); i++) {
+        containers.add(containerTemplate(name: "block${i}", image: blocks[i].dockerImage, ttyEnabled: true, command: 'cat', alwaysPullImage: true))
+    }
+
+    def creds = [string(credentialsId: 'DO_TOKEN', variable: 'DIGITALOCEAN_TOKEN')]
+    withCredentials(creds) {
+        podTemplate(
+            label: nodeName,
+            containers: containers,
+            envVars: [
+                envVar(key: 'TF_VAR_consul_address', value: context.env.TF_VAR_consul_address),
+                secretEnvVar(key: 'DIGITALOCEAN_TOKEN', value: env.DIGITALOCEAN_TOKEN),
+            ],
+        ) {
+            node(nodeName) {
+                for (def i = 0; i < blocks.size(); i++) {
+                    blocks[i].name = "block${i}"
+                    container("block${i}") {
+                        context.pipeline.scmCheckout()
+                        unstash('config')
+                        def block = new DrupipeBlock(blocks[i])
+                        echo 'BLOCK EXECUTE START'
+                        sshagent([context.credentialsId]) {
+                            block.blockInNode = true
+                            context << block.execute(context)
+                        }
+                        echo 'BLOCK EXECUTE END'
+                    }
+                }
+            }
+        }
+    }
+
+    context
+}
