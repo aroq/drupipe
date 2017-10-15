349c09f963771c12e0ba766265a54ef2e8578c49
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-15 16:28:43 +0300

    Add gcloud auth

--- src/com/github/aroq/drupipe/actions/GCloud.groovy
+++ src/com/github/aroq/drupipe/actions/GCloud.groovy
@@ -14,6 +14,21 @@ class GCloud extends BaseAction {
 
     def auth() {
         this.script.echo "gcloud.auth"
+
+        action.params.workingDir = this.script.pwd()
+
+        def creds = [script.file(credentialsId: 'GCLOUD_ACCESS_KEY', variable: 'GCLOUD_ACCESS_KEY')]
+        script.withCredentials(creds) {
+            script.withEnv("KUBECONFIG=${action.params.workingDir}")
+            this.script.drupipeShell("""
+              gcloud auth activate-service-account --key-file ${GCLOUD_ACCESS_KEY}
+              gcloud config set compute/zone europe-west1-b
+              gcloud config set project zebra-aroq
+              gcloud config set container/use_client_certificate True
+              gcloud container clusters get-credentials main
+              ls -al ${action.params.workingDir}
+            """, this.context << [shellCommandWithBashLogin: false])
+        }
     }
 
 }
