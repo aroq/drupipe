167a9c3c93a27f0fc325f8a9fd59cf65bb3a3655
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-15 10:33:32 +0300

    Add Helm action

---
+++ src/com/github/aroq/drupipe/actions/Helm.groovy
@@ -0,0 +1,28 @@
+package com.github.aroq.drupipe.actions
+
+import com.github.aroq.drupipe.DrupipeAction
+
+class Helm extends BaseAction {
+
+    def context
+
+    def script
+
+    def utils
+
+    def DrupipeAction action
+
+    def status() {
+        this.script.echo "Helm.status"
+    }
+
+    def install() {
+        this.script.echo "Helm.install"
+    }
+
+    def delete() {
+        this.script.echo "Helm.delete"
+    }
+
+}
+
