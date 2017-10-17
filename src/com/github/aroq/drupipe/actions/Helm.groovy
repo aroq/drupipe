82a885722e98877d6e537d63d4f8181e46fc48e5
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-17 15:05:07 +0300

    Refactor Helm action

--- src/com/github/aroq/drupipe/actions/Helm.groovy
+++ src/com/github/aroq/drupipe/actions/Helm.groovy
@@ -32,26 +32,25 @@ class Helm extends BaseAction {
         // Execute helm command.
         // TODO: Make sure only allowed credentials could be used. Control it with projects.yaml in mothership config.
         String command = [params.executable, params.command, params.flags, params.release_name, params.chart_dir].join(' ')
-//        def creds = [script.file(credentialsId: params.secret_values_file_id, variable: params.secret_values_file_id)]
 
-        // Process credentials.
-        ArrayList credentials = []
-        if (params.credentials) {
-            params.credentials.each { k, v ->
-                if (v.type == 'file') {
-                    v.variable_name = v.variable_name ? v.variable_name : v.id
-                    credentials << this.script.file(credentialsId: v.id, variable: v.variable_name)
-                }
-            }
-        }
-
-        script.withCredentials(credentials) {
+//        // Process credentials.
+//        ArrayList credentials = []
+//        if (params.credentials) {
+//            params.credentials.each { k, v ->
+//                if (v.type == 'file') {
+//                    v.variable_name = v.variable_name ? v.variable_name : v.id
+//                    credentials << this.script.file(credentialsId: v.id, variable: v.variable_name)
+//                }
+//            }
+//        }
+//
+//        script.withCredentials(credentials) {
             this.script.withEnv(["KUBECONFIG=${params.working_dir}/${params.kubectl_config_file}"]) {
                 this.script.drupipeShell("""
                     ${command} 
                 """, this.context << [shellCommandWithBashLogin: false])
             }
-        }
+//        }
     }
 
     def status() {
