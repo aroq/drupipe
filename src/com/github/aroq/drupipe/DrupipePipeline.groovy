7c0309da33c1350797df842e70a17cb586e3b180
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-24 18:05:49 +0300

    Add kubestl scale

--- src/com/github/aroq/drupipe/DrupipePipeline.groovy
+++ src/com/github/aroq/drupipe/DrupipePipeline.groovy
@@ -255,7 +255,7 @@ class DrupipePipeline implements Serializable {
             actionMethodName = values[0]
         }
 
-        new DrupipeAction(name: actionName, methodName: actionMethodName, params: actionParams, context: context.clone())
+        new DrupipeAction(name: actionName, methodName: actionMethodName, params: actionParams, context: context)
     }
 
     def executePipelineActionList(actions, context) {
