e03ea869d4531c32970737396ff4c05009f2f2df
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-24 18:25:49 +0300

    Add kubestl scale

--- src/com/github/aroq/drupipe/Utils.groovy
+++ src/com/github/aroq/drupipe/Utils.groovy
@@ -525,9 +525,13 @@ def processActionParams(params, context, action, ArrayList prefixes) {
             })
         }
     } else if (params instanceof List) {
-        for (def i = 0; i < params.size(); i++) {
-            params[i] = interpolateCommand(params[i], context, action)
-        }
+//        for (param in params) {
+//            param = interpolateCommand(param, context, action)
+//        }
+//        for (def i = 0; i < params.size(); i++) {
+//            params[i] = interpolateCommand(params[i], context, action)
+//        }
+        params = params.collect { interpolateCommand(it, context, action) }
 
     }
     return params
