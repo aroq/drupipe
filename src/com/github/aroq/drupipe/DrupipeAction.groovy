aa47c1b3b0cf0a448a4f7bdc95dc841e9ac75296
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-24 23:34:57 +0300

    Add kubestl scale

--- src/com/github/aroq/drupipe/DrupipeAction.groovy
+++ src/com/github/aroq/drupipe/DrupipeAction.groovy
@@ -68,12 +68,14 @@ class DrupipeAction implements Serializable {
             }
             this.params = utils.merge(defaultActionParams, this.params)
 
-            def contextParamsConfigFile = ['.unipipe', 'context.yaml'].join('/')
+            if (context.params) {
+                def contextParamsConfigFile = ['.unipipe', 'context.yaml'].join('/')
 //            if (this.context.pipeline.script.fileExists(contextParamsConfigFile)) {
                 this.context.pipeline.script.writeYaml(file: contextParamsConfigFile, data: context.params)
 //            }
+                this.context.pipeline.script.sh("cat ${contextParamsConfigFile}")
+            }
 
-            this.context.pipeline.script.sh("cat ${contextParamsConfigFile}")
 
             // Interpolate action params with context variables.
             if (this.params.containsKey('interpolate') && (this.params.interpolate == 0 || this.params.interpolate == '0')) {
