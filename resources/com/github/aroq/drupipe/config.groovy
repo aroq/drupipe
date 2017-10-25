f00a9bb550a37f24fe937d33f90a17d2fd02d072
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-25 15:14:22 +0300

    Add HealthCheck action wait for http 200

--- resources/com/github/aroq/drupipe/config.groovy
+++ resources/com/github/aroq/drupipe/config.groovy
@@ -337,7 +337,7 @@ params = [
             environment: '',
             chart_name: '',
             release_name: '${action.params.chart_name}-${action.params.environment}',
-            jsonpath: '{.items[0].status.loadBalancer.ingress[0].ip}:{.items[0].spec.ports[?(@.name=="http")].port}',
+            jsonpath: '\'{.items[0].status.loadBalancer.ingress[0].ip}:{.items[0].spec.ports[?(@.name=="http")].port}\'',
             flags: [
                 '--namespace': ['${action.params.namespace}'],
                 '--selector': ['release=${action.params.release_name}'],
