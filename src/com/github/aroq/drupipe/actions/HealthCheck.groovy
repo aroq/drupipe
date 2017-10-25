d344f3fbf6dfef40eb155e76cbdaea4fae5bc118
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-25 12:35:10 +0300

    Add HealthCheck action

---
+++ src/com/github/aroq/drupipe/actions/HealthCheck.groovy
@@ -0,0 +1,19 @@
+package com.github.aroq.drupipe.actions
+
+import com.github.aroq.drupipe.DrupipeAction
+
+class HealthCheck extends BaseAction {
+
+    def context
+
+    def script
+
+    def utils
+
+    DrupipeAction action
+
+    def test_http_ok() {
+    }
+
+}
+
