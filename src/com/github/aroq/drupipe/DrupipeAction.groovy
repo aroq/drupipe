021ffe1d0a2f4cdd79294a080d6b01e232626f61
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-26 18:03:19 +0300

    Add HealthCheck action wait for http 200

--- src/com/github/aroq/drupipe/DrupipeAction.groovy
+++ src/com/github/aroq/drupipe/DrupipeAction.groovy
@@ -175,13 +175,13 @@ class DrupipeAction implements Serializable {
                             if (!context.actions) {
                                 context.actions = [:]
                             }
-//                            context.actions["${name}_${methodName}"] = [
-//                                    params: this.params,
+                            context.actions["${name}_${methodName}"] = [
+                                    params: this.params,
 //                                    result: actionResult,
-//                            ]
+                            ]
 
                             if (context.params && context.params.action && context.params.action["${name}_${methodName}"] && context.params.action["${name}_${methodName}"].debugEnabled) {
-//                                utils.debugLog(context, context.actions, "context actions", [debugMode: 'json'], ["${name}_${methodName}"], true)
+                                utils.debugLog(context, context.actions, "context actions", [debugMode: 'json'], ["${name}_${methodName}"], true)
                             }
 
                         }
