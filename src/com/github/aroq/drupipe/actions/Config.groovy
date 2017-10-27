c5bacacd666bd49a0c40b4f485a15272a5926035
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-27 15:22:11 +0300

    Refactor params

--- src/com/github/aroq/drupipe/actions/Config.groovy
+++ src/com/github/aroq/drupipe/actions/Config.groovy
@@ -189,10 +189,10 @@ class Config extends BaseAction {
                 ]
             ]
             result = context.pipeline.executePipelineActionList(providers, context)
-            def mothershipConfig = this.utils.getMothershipConfigFile(context)
-            def mothershipServers = this.utils.getMothershipServersFile(context)
+            def mothershipConfig = this.utils.getMothershipConfigFile(result)
+            def mothershipServers = this.utils.getMothershipServersFile(result)
 
-            result = utils.merge(result, mothershipConfig[context.jenkinsFolderName])
+            result = utils.merge(result, mothershipConfig[result.jenkinsFolderName])
             result = utils.merge(result, [jenkinsServers: mothershipServers])
 
             this.configRepo = result.configRepo
