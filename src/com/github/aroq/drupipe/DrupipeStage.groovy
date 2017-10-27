b30bdf9183fd5c0d9563a76e09363cea7a68dd58
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-27 13:05:33 +0300

    Refactor params

--- src/com/github/aroq/drupipe/DrupipeStage.groovy
+++ src/com/github/aroq/drupipe/DrupipeStage.groovy
@@ -11,6 +11,7 @@ class DrupipeStage implements Serializable {
     def execute(params, body = null) {
         def utils = new com.github.aroq.drupipe.Utils()
         this.params = params
+        utils.dump(params, this.params, 'DrupipeStage this.params BEFORE', true)
         this.params.pipeline.script.stage(name) {
             this.params.pipeline.script.gitlabCommitStatus(name) {
                 if (body) {
@@ -23,8 +24,10 @@ class DrupipeStage implements Serializable {
                             if (params && params.action && params.action["${a.name}_${a.methodName}"] && params.action["${name}_${a.methodName}"].debugEnabled) {
                                 utils.debugLog(params, this.params, "ACTION ${a.name}.${a.methodName} DrupipeStage.execute() BEFORE EXECUTE", [:], [], true)
                             }
+                            utils.dump(params, this.params, 'DrupipeStage a BEFORE EXECUTE', true)
                             def action = new DrupipeAction(a)
                             this.params << action.execute(this.params)
+                            utils.dump(params, this.params, 'DrupipeStage this.params AFTER', true)
                             if (params && params.action && params.action["${a.name}_${a.methodName}"] && params.action["${name}_${a.methodName}"].debugEnabled) {
                                 utils.debugLog(params, this.params, "ACTION ${a.name}.${a.methodName} DrupipeStage.execute() AFTER EXECUTE", [:], [], true)
                             }
