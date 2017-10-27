89d1cb185212215742129e0f8cafa222391a3dab
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-27 23:57:47 +0300

    Refactor params

--- src/com/github/aroq/drupipe/DrupipeAction.groovy
+++ src/com/github/aroq/drupipe/DrupipeAction.groovy
@@ -14,7 +14,7 @@ class DrupipeAction implements Serializable {
 
     HashMap notification = [:]
 
-    LinkedHashMap context = [:]
+//    LinkedHashMap pipeline.context = [:]
 
     def pipeline
 
@@ -27,7 +27,6 @@ class DrupipeAction implements Serializable {
     }
 
     def execute() {
-        context = pipeline.context
         def utils = new com.github.aroq.drupipe.Utils()
 
         this.script = pipeline.script
@@ -35,17 +34,17 @@ class DrupipeAction implements Serializable {
         try {
             // Stage name & echo.
             String drupipeStageName
-            if (this.context.stage) {
-                drupipeStageName = "${this.context.stage.name}"
+            if (pipeline.context.stage) {
+                drupipeStageName = "${pipeline.context.stage.name}"
             }
             else {
                 drupipeStageName = 'config'
             }
-            this.context.drupipeStageName = drupipeStageName
+            pipeline.context.drupipeStageName = drupipeStageName
             notification.name = "Action ${name}"
             notification.level = "action:${drupipeStageName}"
 
-            utils.pipelineNotify(context, notification << [status: 'START'])
+            utils.pipelineNotify(pipeline.context, notification << [status: 'START'])
             utils.echoDelimiter("-----> DrupipeStage: ${drupipeStageName} | DrupipeAction name: ${this.fullName} start <-")
 
             // Define action params.
@@ -54,22 +53,22 @@ class DrupipeAction implements Serializable {
             def defaultActionParams = [:]
 
             // TODO: read action default params from YAML.
-//            def actionConfigFile = [utils.sourceDir(context, 'library'), 'actions', this.name + '.yaml'].join('/')
+//            def actionConfigFile = [utils.sourceDir(pipeline.context, 'library'), 'actions', this.name + '.yaml'].join('/')
 //            if (this.script.fileExists(actionConfigFile)) {
 //                def actionConfig = this.script.readYaml(file: actionConfigFile)
-//                utils.debugLog(context, actionConfig, "${this.fullName} action YAML CONFIG", [:], [], true)
+//                utils.debugLog(pipeline.context, actionConfig, "${this.fullName} action YAML CONFIG", [:], [], true)
 //            }
 
             if (this.params) {
-                utils.debugLog(context, this.params, "ACTION INIT 1 ${name}.${methodName}", [debugMode: 'json'], [], true)
-                utils.debugLog(context, defaultActionParams, "defaultActionParams INIT 1 ${name}.${methodName}", [debugMode: 'json'], [], true)
+                utils.debugLog(pipeline.context, this.params, "ACTION INIT 1 ${name}.${methodName}", [debugMode: 'json'], [], true)
             }
+            utils.debugLog(pipeline.context, defaultActionParams, "defaultActionParams INIT 1 ${name}.${methodName}", [debugMode: 'json'], [], true)
 
             for (actionName in ['ACTION',this.name, this.name + '_' + this.methodName]) {
-                if (context && context.params && context.params.action && actionName in context.params.action) {
+                if (pipeline.context && pipeline.context.params && pipeline.context.params.action && actionName in pipeline.context.params.action) {
                     script.echo "Merging params from: ${actionName}"
-                    defaultActionParams = utils.merge(defaultActionParams, context.params.action[actionName])
-                    utils.debugLog(context, defaultActionParams, "defaultActionParams init 2 ${name}.${methodName}: ${actionName}", [debugMode: 'json'], [], true)
+                    defaultActionParams = utils.merge(defaultActionParams, pipeline.context.params.action[actionName])
+                    utils.debugLog(pipeline.context, defaultActionParams, "defaultActionParams init 2 ${name}.${methodName}: ${actionName}", [debugMode: 'json'], [], true)
                 }
             }
 
@@ -79,44 +78,44 @@ class DrupipeAction implements Serializable {
             this.params = utils.merge(defaultActionParams, this.params)
 
             if (this.params) {
-                utils.debugLog(context, this.params, "ACTION INIT 3 ${name}.${methodName} AFTER THIS.PARAMS MERGE", [debugMode: 'json'], [], true)
-                utils.debugLog(context, defaultActionParams, "defaultActionParams INIT 4 ${name}.${methodName} AFTER THIS.PARAMS MERGE", [debugMode: 'json'], [], true)
+                utils.debugLog(pipeline.context, this.params, "ACTION INIT 3 ${name}.${methodName} AFTER THIS.PARAMS MERGE", [debugMode: 'json'], [], true)
+                utils.debugLog(pipeline.context, defaultActionParams, "defaultActionParams INIT 4 ${name}.${methodName} AFTER THIS.PARAMS MERGE", [debugMode: 'json'], [], true)
             }
 
-            // Save original (unprocessed) context.params.
+            // Save original (unprocessed) pipeline.context.params.
             // TODO: save only needed actions.
-            def contextParamsConfigFile = ['.unipipe', 'context.params.yaml'].join('/')
-            if (context.params) {
+            def contextParamsConfigFile = ['.unipipe', 'pipeline.context.params.yaml'].join('/')
+            if (pipeline.context.params) {
                 if (this.script.fileExists(contextParamsConfigFile)) {
                     this.script.sh("rm -f ${contextParamsConfigFile}")
                 }
-                this.script.writeYaml(file: contextParamsConfigFile, data: context.params)
-//                utils.debugLog(context, context, "CONTEXT PARAMS result results AFTER SAVE", [debugMode: 'json'], ['params', 'action', 'ACTION', 'results'], true)
+                this.script.writeYaml(file: contextParamsConfigFile, data: pipeline.context.params)
+                utils.debugLog(pipeline.context, pipeline.context, "pipeline.context PARAMS rAFTER SAVE", [debugMode: 'json'], ['params', 'action', 'Config'], true)
             }
 
             if (this.params && this.params.debugEnabled) {
-                utils.debugLog(context, this.params, "ACTION ${name}.${methodName} BEFORE PROCESSING", [debugMode: 'json'], [], true)
-                utils.debugLog(context, context, "${name}.${methodName} BEFORE PROCESSING", [debugMode: 'json'], ['params', 'action', "${name}_${methodName}"], true)
-                utils.debugLog(context, context, "${name} BEFORE PROCESSING", [debugMode: 'json'], ['params', 'action', "${name}"], true)
+                utils.debugLog(pipeline.context, this.params, "ACTION ${name}.${methodName} BEFORE PROCESSING", [debugMode: 'json'], [], true)
+                utils.debugLog(pipeline.context, pipeline.context, "${name}.${methodName} BEFORE PROCESSING", [debugMode: 'json'], ['params', 'action', "${name}_${methodName}"], true)
+                utils.debugLog(pipeline.context, pipeline.context, "${name} BEFORE PROCESSING", [debugMode: 'json'], ['params', 'action', "${name}"], true)
             }
 
-            // Interpolate action params with context variables.
+            // Interpolate action params with pipeline.context variables.
             if (this.params.containsKey('interpolate') && (this.params.interpolate == 0 || this.params.interpolate == '0')) {
                 this.script.echo "Action ${this.fullName}: Interpolation disabled by interpolate config directive."
             }
             else {
-                utils.processActionParams(this, context, [this.name.toUpperCase(), (this.name + '_' + this.methodName).toUpperCase()])
-                // TODO: Store processed action params in context (context.actions['action_name']) to allow use it for interpolation in other actions.
+                utils.processActionParams(this, pipeline.context, [this.name.toUpperCase(), (this.name + '_' + this.methodName).toUpperCase()])
+                // TODO: Store processed action params in pipeline.context (pipeline.context.actions['action_name']) to allow use it for interpolation in other actions.
             }
 
             if (this.params && this.params.debugEnabled) {
-                utils.debugLog(context, this.params, "ACTION ${name}.${methodName} AFTER PROCESSING", [debugMode: 'json'], [], true)
-                utils.debugLog(context, context, "${name}.${methodName} AFTER PROCESSING", [debugMode: 'json'], ['params', 'action', "${name}_${methodName}"], true)
-//                utils.debugLog(context, context, "${name} AFTER PROCESSING", [debugMode: 'json'], ['params', 'action', "${name}"], true)
+                utils.debugLog(pipeline.context, this.params, "ACTION ${name}.${methodName} AFTER PROCESSING", [debugMode: 'json'], [], true)
+                utils.debugLog(pipeline.context, pipeline.context, "${name}.${methodName} AFTER PROCESSING", [debugMode: 'json'], ['params', 'action', "${name}_${methodName}"], true)
+//                utils.debugLog(pipeline.context, pipeline.context, "${name} AFTER PROCESSING", [debugMode: 'json'], ['params', 'action', "${name}"], true)
             }
 
             actionParams << this.params
-//            utils.debugLog(context, actionParams, "${this.fullName} action params")
+//            utils.debugLog(pipeline.context, actionParams, "${this.fullName} action params")
 
             def actionFile = null
 
@@ -136,13 +135,13 @@ class DrupipeAction implements Serializable {
                 def envParams = actionParams.env ? actionParams.env.collect{ k, v -> "$k=$v"} : []
                 script.withEnv(envParams) {
                     // Execute action from file if exist in sources...
-                    if (context.sourcesList) {
-                        for (def i = 0; i < context.sourcesList.size(); i++) {
-                            def source = context.sourcesList[i]
-                            def fileName = utils.sourcePath(context, source.name, 'pipelines/actions/' + this.name + '.groovy')
+                    if (pipeline.context.sourcesList) {
+                        for (def i = 0; i < pipeline.context.sourcesList.size(); i++) {
+                            def source = pipeline.context.sourcesList[i]
+                            def fileName = utils.sourcePath(pipeline.context, source.name, 'pipelines/actions/' + this.name + '.groovy')
                             utils.debugLog(actionParams, fileName, "DrupipeAction file name to check")
                             // To make sure we only check fileExists in Heavyweight executor mode.
-                            if (context.block?.nodeName && this.script.fileExists(fileName)) {
+                            if (pipeline.context.block?.nodeName && this.script.fileExists(fileName)) {
                                 actionFile = this.script.load(fileName)
                                 // TODO: Add timeout.
                                 this.result.action_result = actionFile."${this.methodName}"(actionParams)
@@ -153,7 +152,7 @@ class DrupipeAction implements Serializable {
                     if (!actionFile) {
                         try {
                             def actionInstance = this.class.classLoader.loadClass("com.github.aroq.drupipe.actions.${this.name}", true, false )?.newInstance(
-                                context: context.clone(),
+                                pipeline.context: pipeline.context.clone(),
                                 action: this,
                                 script: this.script,
                                 utils: utils,
@@ -172,7 +171,7 @@ class DrupipeAction implements Serializable {
 
                     // Results processing.
                     if (this.params.store_action_params) {
-                        contextStoreResult(this.params.store_action_params_key.tokenize('.'), context, this.params)
+                        contextStoreResult(this.params.store_action_params_key.tokenize('.'), pipeline.context, this.params)
                     }
                     if (this.params.store_result) {
                         if (this.params.post_process) {
@@ -183,7 +182,7 @@ class DrupipeAction implements Serializable {
                                 }
                                 if (result.value.type == 'result') {
                                     if (this.params.dump_result) {
-                                        utils.debugLog(context, this.result.action_result, "action_result", [debugMode: 'json'], [], true)
+                                        utils.debugLog(pipeline.context, this.result.action_result, "action_result", [debugMode: 'json'], [], true)
                                     }
                                     script.echo "SOURCE: ${result.value.source}"
                                     def deepValue = utils.deepGet(this.result.action_result, result.value.source.tokenize('.'))
@@ -192,7 +191,7 @@ class DrupipeAction implements Serializable {
                                         contextStoreResult(result.value.destination.tokenize('.'), this.result, deepValue)
                                         if (this.params.dump_result) {
                                             script.echo "deepValue: ${deepValue}"
-//                                            utils.debugLog(context, this.result.action_result, "action_result after result save", [debugMode: 'json'], [], true)
+//                                            utils.debugLog(pipeline.context, this.result.action_result, "action_result after result save", [debugMode: 'json'], [], true)
                                         }
                                     }
                                 }
@@ -200,8 +199,8 @@ class DrupipeAction implements Serializable {
                         }
                     }
 
-                    if (context.params && context.params.action && context.params.action["${name}_${methodName}"] && context.params.action["${name}_${methodName}"].debugEnabled) {
-                        utils.debugLog(context, context, "context results", [debugMode: 'json'], [this.params.store_result_key], true)
+                    if (pipeline.context.params && pipeline.context.params.action && pipeline.context.params.action["${name}_${methodName}"] && pipeline.context.params.action["${name}_${methodName}"].debugEnabled) {
+                        utils.debugLog(pipeline.context, pipeline.context, "pipeline.context results", [debugMode: 'json'], [this.params.store_result_key], true)
                     }
                 }
             }
@@ -215,18 +214,18 @@ class DrupipeAction implements Serializable {
 //                def path = storeResult.tokenize('.')
 //                def stored = [:]
 //                contextStoreResult(path, stored, this.result.action_result.stdout)
-//                this.context.pipeline.script.echo("STORED-RESULT: ${stored}")
+//                pipeline.context.pipeline.script.echo("STORED-RESULT: ${stored}")
 //
 //                action_result << stored
 //            }
 
-            // Restore original (unprocessed) context.params.
+            // Restore original (unprocessed) pipeline.context.params.
             // TODO: restore only needed actions.
-            if (context.params) {
+            if (pipeline.context.params) {
                 if (script.fileExists(contextParamsConfigFile)) {
-                    this.context.params = script.readYaml(file: contextParamsConfigFile)
+                    pipeline.context.params = script.readYaml(file: contextParamsConfigFile)
                 }
-                utils.debugLog(context, context, "CONTEXT PARAMS result results AFTER RESTORE", [debugMode: 'json'], ['params', 'action', 'ACTION', 'results'], true)
+                utils.debugLog(pipeline.context, pipeline.context, "pipeline.context PARAMS result results AFTER RESTORE", [debugMode: 'json'], ['params', 'action', 'ACTION', 'results'], true)
             }
 
 //            this.result.action_result = action_result
@@ -250,7 +249,7 @@ class DrupipeAction implements Serializable {
                 notification.message = notification.message ? notification.message : ''
                 notification.message = notification.message + "\n\n" + this.result.action_result.stdout
             }
-            utils.pipelineNotify(context, notification)
+            utils.pipelineNotify(pipeline.context, notification)
         }
     }
 
