9a9907391f31b40e45a4f84de60e1abe2c6a384b
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-30 20:31:45 +0300

    Refactor params

--- src/com/github/aroq/drupipe/actions/DrushFeaturesList.groovy
+++ src/com/github/aroq/drupipe/actions/DrushFeaturesList.groovy
@@ -4,21 +4,19 @@ import com.github.aroq.drupipe.DrupipeActionWrapper
 
 class DrushFeaturesList extends BaseAction {
 
-    def context
-
     def script
 
     def utils
 
     String state = 'Overridden'
 
-    def DrupipeActionWrapper action
+    DrupipeActionWrapper action
 
     def runCommand() {
         def features = []
 
-        this.context.drush_command = 'fl --format=json'
-        def fl_result = this.script.drupipeAction([action: "Drush.runCommand", params: [store_result: true]], this.context)
+        action.pipeline.context.drush_command = 'fl --format=json'
+        def fl_result = this.script.drupipeAction([action: "Drush.runCommand", params: [store_result: true]], action.pipeline.context)
 
         def jsonOutput = this.script.readJSON(text: fl_result.stdout)
         jsonOutput.each { key, feature->
@@ -32,8 +30,8 @@ class DrushFeaturesList extends BaseAction {
             features.each { feature ->
                 exception_table = exception_table + "|${feature['name']}|${feature['feature']}|\n"
 
-                this.context.drush_command = 'fd ' + feature['feature']
-                this.script.drupipeAction("Drush.runCommand", this.context)
+                action.pipeline.context.drush_command = 'fd ' + feature['feature']
+                this.script.drupipeAction("Drush.runCommand", action.pipeline.context)
             }
 
             throw new Exception("OVERRIDEN FEATURES:\n\n${exception_table}")
