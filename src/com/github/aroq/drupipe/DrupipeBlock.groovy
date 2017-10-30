21ac1a5bb98d421dfc6781cf4845e3720b994a76
Alexander Tolstikov <tolstikov@gmail.com>
2017-10-30 17:51:06 +0300

    Refactor params

--- src/com/github/aroq/drupipe/DrupipeBlock.groovy
+++ src/com/github/aroq/drupipe/DrupipeBlock.groovy
@@ -46,6 +46,9 @@ class DrupipeBlock implements Serializable {
             //utils.pipelineNotify(pipeline.context, [name: "Block on ${nodeName}", status: 'START', level: 'block'])
             pipeline.script.echo "NODE NAME: ${nodeName}"
             pipeline.script.node(nodeName) {
+                pipeline.context = utils.serializeAndDeserialize(pipeline.context)
+                utils.debugLog(pipeline.context, pipeline.context, "CONFIG CONTEXT - DrupipeBlock - 1", [debugMode: 'json'], [], true)
+
                 // Secret option for emergency remove workspace.
                 if (pipeline.context.force == '11') {
                     pipeline.script.echo 'FORCE REMOVE DIR'
