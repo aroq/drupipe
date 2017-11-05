2661b972c19c91ab3e6e91b8d5a210e5cc2963e2
Alexander Tolstikov <tolstikov@gmail.com>
2017-11-05 20:53:53 +0300

    Parameter separator

--- jobdsl/seed/job_dsl_jobs.groovy
+++ jobdsl/seed/job_dsl_jobs.groovy
@@ -678,18 +678,18 @@ def drupipeParamsDefault(context, job, config) {
     context.stringParam('debugEnabled', '0')
     context.stringParam('force', '0')
 
-    drupipeParameterSeparatorStylized(context, 'default_params_test', 'Block parameters', 'green', false, '1px', '14px')
+    drupipeParameterSeparatorStylized(context, 'default_params_test', 'Block parameters', 'green', true, '1px', '14px')
 
     drupipeParamNodeNameSelects(context, job, config)
     drupipeParamDisableBlocksCheckboxes(context, job)
 
     if (job.value.containsKey('notify')) {
-        drupipeParameterSeparatorStylized(context, 'default_notification_params', 'Notification parameters', 'green', false,'1px', '14px')
+        drupipeParameterSeparatorStylized(context, 'default_notification_params', 'Notification parameters', 'green', true,'1px', '14px')
         drupipeParamMuteNotificationCheckboxes(context, job)
     }
 
     if (job.value.containsKey('trigger')) {
-        drupipeParameterSeparatorStylized(context, 'default_trigger_params', 'Trigger parameters', 'green', false,'1px', '14px')
+        drupipeParameterSeparatorStylized(context, 'default_trigger_params', 'Trigger parameters', 'green', true,'1px', '14px')
         drupipeParamDisableTriggersCheckboxes(context, job)
         drupipeParamTriggerParams(context, job)
     }
