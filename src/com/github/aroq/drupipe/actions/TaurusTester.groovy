92baddc9eb4e87108ffd8403cd660e07049fe87f
Alexander Tolstikov <tolstikov@gmail.com>
2017-11-06 14:49:24 +0300

    Fix taurus

--- src/com/github/aroq/drupipe/actions/TaurusTester.groovy
+++ src/com/github/aroq/drupipe/actions/TaurusTester.groovy
@@ -4,13 +4,11 @@ import com.github.aroq.drupipe.DrupipeActionWrapper
 
 class TaurusTester extends BaseAction {
 
-    def context
-
     def script
 
     def utils
 
-    def DrupipeActionWrapper action
+    DrupipeActionWrapper action
 
     def test() {
         this.script.dir("logs") {
@@ -19,21 +17,21 @@ class TaurusTester extends BaseAction {
 
         this.script.checkout this.script.scm
 
-        def hold_for = (this.context.taurus_hold_for && this.context.taurus_hold_for.length() != 0) ? "-o execution.hold-for=${this.context.taurus_hold_for}" : ''
-        def ramp_up = (this.context.taurus_ramp_up && this.context.taurus_ramp_up.length() != 0) ? "-o execution.ramp-up=${this.context.taurus_ramp_up}" : ''
-        def concurrency = (this.context.taurus_concurrency && this.context.taurus_concurrency.length() != 0) ? "-o execution.concurrency=${this.context.taurus_concurrency}" : ''
-        def throughput = (this.context.taurus_throughput && this.context.taurus_throughput.length() != 0) ? "-o execution.throughput=${this.context.taurus_throughput}" : ''
-        def steps = (this.context.taurus_steps && this.context.taurus_steps.length() != 0) ? "-o execution.steps=${this.context.taurus_steps}" : ''
-        def iterations = (this.context.taurus_iterations && this.context.taurus_iterations.length() != 0) ? "-o execution.iterations=${this.context.taurus_iterations}" : ''
+        def hold_for = (action.pipeline.context.taurus_hold_for && action.pipeline.context.taurus_hold_for.length() != 0) ? "-o execution.hold-for=${action.pipeline.context.taurus_hold_for}" : ''
+        def ramp_up = (action.pipeline.context.taurus_ramp_up && action.pipeline.context.taurus_ramp_up.length() != 0) ? "-o execution.ramp-up=${action.pipeline.context.taurus_ramp_up}" : ''
+        def concurrency = (action.pipeline.context.taurus_concurrency && action.pipeline.context.taurus_concurrency.length() != 0) ? "-o execution.concurrency=${action.pipeline.context.taurus_concurrency}" : ''
+        def throughput = (action.pipeline.context.taurus_throughput && action.pipeline.context.taurus_throughput.length() != 0) ? "-o execution.throughput=${action.pipeline.context.taurus_throughput}" : ''
+        def steps = (action.pipeline.context.taurus_steps && action.pipeline.context.taurus_steps.length() != 0) ? "-o execution.steps=${action.pipeline.context.taurus_steps}" : ''
+        def iterations = (action.pipeline.context.taurus_iterations && action.pipeline.context.taurus_iterations.length() != 0) ? "-o execution.iterations=${action.pipeline.context.taurus_iterations}" : ''
 
-        def bztString = """${this.context.taurus_config} \
+        def bztString = """${action.pipeline.context.taurus_config} \
 ${hold_for} \
 ${ramp_up} \
 ${concurrency} \
 ${throughput} \
 ${steps} \
 ${iterations} \
-${this.context.taurus_args}"""
+${action.pipeline.context.taurus_args}"""
 
         this.script.echo "Execute BZT: ${bztString}"
 
