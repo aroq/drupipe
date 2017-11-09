fdfa4c7213c190188ac4b1364478b09e6b7060e4
Alexander Tolstikov <tolstikov@gmail.com>
2017-11-09 22:00:41 +0300

    Disable gcloud auth in k8s mode

--- src/com/github/aroq/drupipe/actions/GCloud.groovy
+++ src/com/github/aroq/drupipe/actions/GCloud.groovy
@@ -13,29 +13,34 @@ class GCloud extends BaseAction {
     DrupipeActionWrapper action
 
     def auth() {
-        // TODO: Make it centralized.
-        String access_key_file
-        if (action.pipeline.context.containerMode == 'kubernetes') {
-            this.script.drupipeShell(
+        if (action.pipeline.context.containerMode != 'kubernetes') {
+
+            // TODO: Make it centralized.
+            String access_key_file
+            if (action.pipeline.context.containerMode == 'kubernetes') {
+
+                this.script.drupipeShell(
 """
 echo "\${${action.params.access_key_file_id}}" > .google_access_key_file
+""", this.action.params)
+
+                access_key_file = action.params.workingDir != '.' ? '.google_access_key_file' : '.google_access_key_file'
+            }
+            else {
+                access_key_file = "\${${action.params.access_key_file_id}}"
+            }
+
+            this.script.drupipeShell(
 """
-                , this.action.params
-            )
-            access_key_file = action.params.workingDir != '.' ? '.google_access_key_file' : '.google_access_key_file'
-        }
-        else {
-            access_key_file = "\${${action.params.access_key_file_id}}"
-        }
-         this.script.drupipeShell("""
-              ${action.params.executable} auth activate-service-account --key-file ${access_key_file}
+${action.params.executable} auth activate-service-account --key-file ${access_key_file}
+${action.params.executable} config set compute/zone ${action.params.compute_zone}
+${action.params.executable} config set project ${action.params.project_name} 
+${action.params.executable} config set container/use_client_certificate True
+${action.params.executable} container clusters get-credentials ${action.params.cluster_name} 
+""", action.params)
 
-              ${action.params.executable} config set compute/zone ${action.params.compute_zone}
-              ${action.params.executable} config set project ${action.params.project_name} 
-              ${action.params.executable} config set container/use_client_certificate True
+        }
 
-              ${action.params.executable} container clusters get-credentials ${action.params.cluster_name} 
-        """, action.params)
     }
 
 }
