857ac830e5cb4d7b4e87a952355c7586380ccf8c
Alexander Tolstikov <tolstikov@gmail.com>
2017-11-11 12:56:59 +0300

    Refactor config (node->container->block)

--- src/com/github/aroq/drupipe/DrupipeContainerBlock.groovy
+++ src/com/github/aroq/drupipe/DrupipeContainerBlock.groovy
@@ -12,9 +12,8 @@ class DrupipeContainerBlock implements Serializable {
         def script = controller.script
         script.echo "DrupipeAction execute - ${name}"
         for (actionWrapper in actions) {
-            actionWrapper = new DrupipeActionWrapper(action)
             actionWrapper.pipeline = controller
-            actionWrapper.execute()
+            (new DrupipeActionWrapper(actionWrapper)).execute()
         }
     }
 
