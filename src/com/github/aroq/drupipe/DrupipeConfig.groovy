35963bf2bbc8cadc474566bc71e184c4c60d20c2
Alexander Tolstikov <tolstikov@gmail.com>
2017-11-16 13:55:38 +0300

    Refactor config (node->container->block)

--- src/com/github/aroq/drupipe/DrupipeConfig.groovy
+++ src/com/github/aroq/drupipe/DrupipeConfig.groovy
@@ -1,7 +1,5 @@
 package com.github.aroq.drupipe
 
-import com.github.aroq.drupipe.processors.DrupipeFromProcessor
-
 class DrupipeConfig {
 
     DrupipeController controller
@@ -10,22 +8,17 @@ class DrupipeConfig {
 
     def utils
 
-
     def config(params) {
         utils = controller.utils
         script.node('master') {
             script.echo "Executing pipeline"
             params.debugEnabled = params.debugEnabled && params.debugEnabled != '0' ? true : false
-
             utils.dump(params, params, 'PIPELINE-PARAMS')
-//            utils.dump(params, config, 'PIPELINE-CONFIG')
-
             // Get config (context).
             script.drupipeAction([action: 'Config.perform', params: [jenkinsParams: params]], controller)
             controller.archiveObjectJsonAndYaml(controller.context, 'context')
         }
     }
-
 
     int configVersion() {
         controller.context.config_version as int
