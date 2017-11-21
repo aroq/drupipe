500f77f791fef1f65066701ac07af9d31e8e6b4a
Alexander Tolstikov <tolstikov@gmail.com>
2017-11-21 12:29:24 +0300

    Add uniconf config & refactor keys

--- vars/drupipeExecuteKubernetesContainers.groovy
+++ vars/drupipeExecuteKubernetesContainers.groovy
@@ -15,15 +15,15 @@ def call(ArrayList containers, DrupipeController controller) {
             echo "Create k8s containerTemplate for container: ${container.name}, image: ${container.image}"
             containerNames += containerName
             containersToExecute.add(containerTemplate(
-                name: containerName,
-                image: container.image,
-                ttyEnabled: container.ttyEnabled,
-                command: container.command,
-                resourceRequestCpu: container.resourceRequestCpu,
-                resourceLimitCpu: container.resourceLimitCpu,
-                resourceRequestMemory: container.resourceRequestMemory,
-                resourceLimitMemory: container.resourceLimitMemory,
-                alwaysPullImage: container.alwaysPullImage,
+                name:                  containerName,
+                image:                 container.image,
+                ttyEnabled:            container.k8s.ttyEnabled,
+                command:               container.k8s.command,
+                resourceRequestCpu:    container.k8s.resourceRequestCpu,
+                resourceLimitCpu:      container.k8s.resourceLimitCpu,
+                resourceRequestMemory: container.k8s.resourceRequestMemory,
+                resourceLimitMemory:   container.k8s.resourceLimitMemory,
+                alwaysPullImage:       container.k8s.alwaysPullImage,
                 // TODO: move in configs.
                 envVars: [
                     envVar(key: 'TF_VAR_consul_address', value: controller.context.env.TF_VAR_consul_address),
@@ -38,32 +38,32 @@ def call(ArrayList containers, DrupipeController controller) {
         }
     }
 
-    def creds = [string(credentialsId: 'DO_TOKEN', variable: 'DIGITALOCEAN_TOKEN')]
-    withCredentials(creds) {
-        podTemplate(
-            label: nodeName,
-            containers: containersToExecute,
-        ) {
-            node(nodeName) {
-                controller.scmCheckout()
-                controller.context.workspace = pwd()
-                for (def i = 0; i < containers.size(); i++) {
-                    container(containers[i].name.replaceAll('\\.','-').replaceAll('_','-')) {
-                        unstash('config')
+//    def creds = [string(credentialsId: 'DO_TOKEN', variable: 'DIGITALOCEAN_TOKEN')]
+//    withCredentials(creds) {
+    podTemplate(
+        label: nodeName,
+        containers: containersToExecute,
+    ) {
+        node(nodeName) {
+            controller.scmCheckout()
+            controller.context.workspace = pwd()
+            for (def i = 0; i < containers.size(); i++) {
+                container(containers[i].name.replaceAll('\\.','-').replaceAll('_','-')) {
+                    unstash('config')
 
-                        for (block in containers[i].blocks) {
+                    for (block in containers[i].blocks) {
 //                            controller.utils.debugLog(controller.context, block, 'CONTAINER BLOCK', [debugMode: 'json'], [], true)
-                            sshagent([controller.context.credentialsId]) {
-                                def drupipeContainerBlock = new DrupipeContainerBlock(block)
-                                drupipeContainerBlock.controller = controller
-                                drupipeContainerBlock.execute()
-                            }
+                        sshagent([controller.context.credentialsId]) {
+                            def drupipeContainerBlock = new DrupipeContainerBlock(block)
+                            drupipeContainerBlock.controller = controller
+                            drupipeContainerBlock.execute()
                         }
                     }
                 }
             }
         }
     }
+//    }
 
 }
 
