ddd4f345cedb4fdef55269161739808a3449cc32
Alexander Tolstikov <tolstikov@gmail.com>
2017-11-21 15:04:04 +0300

    Add uniconf config & refactor keys

--- src/com/github/aroq/drupipe/actions/Config.groovy
+++ src/com/github/aroq/drupipe/actions/Config.groovy
@@ -425,7 +425,7 @@ class Config extends BaseAction {
             def sources = [:]
             if (action.pipeline.context.env.containsKey('UNIPIPE_FROM')) {
                 utils.log "Processing UNIPIPE_FROM"
-                def uniconfSourcesKey = utils.deepGet(action.pipeline.context, 'uniconf.keys.sources')
+                def uniconfSourcesKey = utils.deepGet(projectConfigContext, 'uniconf.keys.sources')
                 sources[uniconfSourcesKey] = script.readJSON(text: action.pipeline.context.env['UNIPIPE_FROM'])
 //                if (projectConfig[uniconfSourcesKey]) {
 //                    projectConfig[uniconfSourcesKey] << sources
