80d4be12802ce72ca421defbbea08a2d427d83f3
Alexander Tolstikov <tolstikov@gmail.com>
2017-12-01 13:06:07 +0300

    Debug kubectl copy file

--- resources/com/github/aroq/drupipe/config.groovy
+++ resources/com/github/aroq/drupipe/config.groovy
@@ -237,219 +237,219 @@ params = [
             jenkins_address: '',
             jenkins_user_token: '',
         ],
-        Helm: [
-            executable: 'helm',
-            chart_name: '', // HELM_CHART_NAME in Jenkins params.
-            release_name: '${action.params.chart_name}-${context.environment}',
-            charts_dir: 'charts',
-            kubectl_config_file: '.kubeconfig',
-            shell_bash_login: false,
-            namespace: '${action.params.chart_name}-${context.environment}',
-            env: [
-                KUBECONFIG: '${context.drupipe_working_dir}/${action.params.kubectl_config_file}'
-            ],
-            post_process: [
-                namespace: [
-                    type: 'result',
-                    source: 'params.namespace', // From action params.
-                    destination: 'context.k8s.namespace',  // To "context".
-                ],
-                release_name: [
-                    type: 'result',
-                    source: 'params.release_name', // From action params.
-                    destination: 'context.k8s.selector',  // To "context".
-                ],
-            ],
-        ],
-        Helm_init: [
-            command: 'init',
-            full_command: [
-                '${action.params.executable}',
-                '${action.params.command}',
-            ],
-        ],
-        Helm_apply: [
-            debugEnabled: true,
-            command: 'upgrade',
-            value_suffix: 'values.yaml',
-            timeout: '120',
-            values_file: '${action.params.chart_name}.${action.params.value_suffix}',
-            env_values_file: '${context.environment}.${action.params.values_file}',
-            secret_values_file_id: '',
-            secret_values_file: '\\\$${action.params.secret_values_file_id}', // To interpolate first "$" sign inside shell script.
-            chart_dir: '${action.params.charts_dir}/${action.params.chart_name}',
-            credentials: [
-                secret_values_file: [
-                    type: 'file',
-                    id: '${action.params.secret_values_file_id}',
-                ],
-            ],
-            flags: [
-                '--install': [''],
-                '--wait': [''],
-                '--timeout': ['${action.params.timeout}'],
-                '--namespace': ['${action.params.namespace}'],
-                // TODO: Files are REQUIRED now. Need to add checks in flags processing to make files optional.
-                '-f': [
-                    '${action.params.values_file}',
-                    '${action.params.env_values_file}',
-                    '${action.params.secret_values_file}',
-                ]
-            ],
-            full_command: [
-                '${action.params.executable}',
-                '${action.params.command}',
-                '${prepareFlags(action.params.flags)}',
-                '${action.params.release_name}',
-                '${action.params.chart_dir}',
-            ],
-        ],
-        Helm_status: [
-            command: 'status',
-            flags: [:],
-            full_command: [
-                '${action.params.executable}',
-                '${action.params.command}',
-                '${action.params.release_name}',
-            ],
-        ],
-        Helm_delete: [
-            command: 'delete',
-            flags: [
-                '--purge': [''],
-            ],
-            full_command: [
-                '${action.params.executable}',
-                '${action.params.command}',
-                '${prepareFlags(action.params.flags)}',
-                '${action.params.release_name}',
-            ],
-        ],
-        Kubectl: [
-            executable: 'kubectl',
-            kubectl_config_file: '.kubeconfig',
-            shell_bash_login: false,
-            namespace: '${context.k8s.namespace}',
-            env: [
-                KUBECONFIG: '${context.drupipe_working_dir}/${action.params.kubectl_config_file}'
-            ],
-        ],
-        Kubectl_scale_replicaset: [
-            command: 'scale replicaset',
-            replicas: '',
-            name: '',
-            flags: [
-                '--replicas': ['${action.params.replicas}'],
-                '--namespace': ['${action.params.namespace}'],
-            ],
-            full_command: [
-                '${action.params.executable}',
-                '${action.params.command}',
-                '${prepareFlags(action.params.flags)}',
-                '${action.params.name}',
-            ],
-        ],
-        Kubectl_scale_down_up: [
-            replicas_down: '0',
-            replicas_up: '1',
-        ],
-        Kubectl_get_replicaset_name: [
-            command: 'get replicaset',
-            release_name: '${actions.Helm_status.release_name}',
-            jsonpath: '\'{.items[0].metadata.name}\'',
-            return_stdout: true,
-            selector: 'release=${context.k8s.selector}',
-            flags: [
-                '--namespace': ['${action.params.namespace}'],
-                '--selector': ['${action.params.selector}'],
-                '-o': ['jsonpath=${action.params.jsonpath}'],
-            ],
-            full_command: [
-                '${action.params.executable}',
-                '${action.params.command}',
-                '${prepareFlags(action.params.flags)}',
-            ],
-        ],
-        Kubectl_get_pod_name: [
-            command: 'get pod',
-            release_name: '${actions.Helm_status.release_name}',
-            jsonpath: '\'{.items[0].metadata.name}\'',
-            return_stdout: true,
-            selector: 'release=${context.k8s.selector}',
-            flags: [
-                '--namespace': ['${action.params.namespace}'],
-                '--selector': ['${action.params.selector}'],
-                '-o': ['jsonpath=${action.params.jsonpath}'],
-            ],
-            full_command: [
-                '${action.params.executable}',
-                '${action.params.command}',
-                '${prepareFlags(action.params.flags)}',
-            ],
-        ],
-        Kubectl_get_pods: [
-            command: 'get pods',
-            flags: [
-                '--namespace': ['${action.params.namespace}'],
-            ],
-            full_command: [
-                '${action.params.executable}',
-                '${action.params.command}',
-                '${prepareFlags(action.params.flags)}',
-            ],
-        ],
-        Kubectl_get_loadbalancer_address: [
-            command: 'get service',
-            release_name: '${actions.Helm_status.release_name}',
-            jsonpath: '\'{.items[0].status.loadBalancer.ingress[0].ip}:{.items[0].spec.ports[?(@.name=="http")].port}\'',
-            return_stdout: true,
-            selector: 'release=${context.k8s.selector}',
-            flags: [
-                '--namespace': ['${action.params.namespace}'],
-                '--selector': ['${action.params.selector}'],
-                '-o': ['jsonpath=${action.params.jsonpath}'],
-            ],
-            full_command: [
-                '${action.params.executable}',
-                '${action.params.command}',
-                '${prepareFlags(action.params.flags)}',
-            ],
-        ],
-        Kubectl_copy_from_pod: [
-            command: 'cp',
-            name: '',
-            source_file_name: '',
-            source: '${context.k8s.namespace}/${action.params.name}:${action.params.source_file_name}',
-            destination: '',
-            full_command: [
-                '${action.params.executable}',
-                '${action.params.command}',
-                '${action.params.source}',
-                '${action.params.destination}',
-            ],
-        ],
-        Kubectl_get_pod_logs: [
-            command: 'logs',
-            name: '',
-            flags: [
-                '--namespace': ['${action.params.namespace}'],
-            ],
-            full_command: [
-                '${action.params.executable}',
-                '${action.params.command}',
-                '${action.params.name}',
-                '${prepareFlags(action.params.flags)}',
-            ],
-        ],
-        HealthCheck_wait_http_ok: [
-            action_timeout: 5,
-            url: '',
-            http_code: '200',
-            interval: '5',
-            command: '''bash -c 'while [[ "\\\$(curl -s -o /dev/null -w ''%{http_code}'' ${action.params.url})" != "${action.params.http_code}" ]]; do sleep ${action.params.interval}; done' ''',
-            full_command: [
-                '${action.params.command}',
-            ],
-        ],
+//        Helm: [
+//            executable: 'helm',
+//            chart_name: '', // HELM_CHART_NAME in Jenkins params.
+//            release_name: '${action.params.chart_name}-${context.environment}',
+//            charts_dir: 'charts',
+//            kubectl_config_file: '.kubeconfig',
+//            shell_bash_login: false,
+//            namespace: '${action.params.chart_name}-${context.environment}',
+//            env: [
+//                KUBECONFIG: '${context.drupipe_working_dir}/${action.params.kubectl_config_file}'
+//            ],
+//            post_process: [
+//                namespace: [
+//                    type: 'result',
+//                    source: 'params.namespace', // From action params.
+//                    destination: 'context.k8s.namespace',  // To "context".
+//                ],
+//                release_name: [
+//                    type: 'result',
+//                    source: 'params.release_name', // From action params.
+//                    destination: 'context.k8s.selector',  // To "context".
+//                ],
+//            ],
+//        ],
+//        Helm_init: [
+//            command: 'init',
+//            full_command: [
+//                '${action.params.executable}',
+//                '${action.params.command}',
+//            ],
+//        ],
+//        Helm_apply: [
+//            debugEnabled: true,
+//            command: 'upgrade',
+//            value_suffix: 'values.yaml',
+//            timeout: '120',
+//            values_file: '${action.params.chart_name}.${action.params.value_suffix}',
+//            env_values_file: '${context.environment}.${action.params.values_file}',
+//            secret_values_file_id: '',
+//            secret_values_file: '\\\$${action.params.secret_values_file_id}', // To interpolate first "$" sign inside shell script.
+//            chart_dir: '${action.params.charts_dir}/${action.params.chart_name}',
+//            credentials: [
+//                secret_values_file: [
+//                    type: 'file',
+//                    id: '${action.params.secret_values_file_id}',
+//                ],
+//            ],
+//            flags: [
+//                '--install': [''],
+//                '--wait': [''],
+//                '--timeout': ['${action.params.timeout}'],
+//                '--namespace': ['${action.params.namespace}'],
+//                // TODO: Files are REQUIRED now. Need to add checks in flags processing to make files optional.
+//                '-f': [
+//                    '${action.params.values_file}',
+//                    '${action.params.env_values_file}',
+//                    '${action.params.secret_values_file}',
+//                ]
+//            ],
+//            full_command: [
+//                '${action.params.executable}',
+//                '${action.params.command}',
+//                '${prepareFlags(action.params.flags)}',
+//                '${action.params.release_name}',
+//                '${action.params.chart_dir}',
+//            ],
+//        ],
+//        Helm_status: [
+//            command: 'status',
+//            flags: [:],
+//            full_command: [
+//                '${action.params.executable}',
+//                '${action.params.command}',
+//                '${action.params.release_name}',
+//            ],
+//        ],
+//        Helm_delete: [
+//            command: 'delete',
+//            flags: [
+//                '--purge': [''],
+//            ],
+//            full_command: [
+//                '${action.params.executable}',
+//                '${action.params.command}',
+//                '${prepareFlags(action.params.flags)}',
+//                '${action.params.release_name}',
+//            ],
+//        ],
+//        Kubectl: [
+//            executable: 'kubectl',
+//            kubectl_config_file: '.kubeconfig',
+//            shell_bash_login: false,
+//            namespace: '${context.k8s.namespace}',
+//            env: [
+//                KUBECONFIG: '${context.drupipe_working_dir}/${action.params.kubectl_config_file}'
+//            ],
+//        ],
+//        Kubectl_scale_replicaset: [
+//            command: 'scale replicaset',
+//            replicas: '',
+//            name: '',
+//            flags: [
+//                '--replicas': ['${action.params.replicas}'],
+//                '--namespace': ['${action.params.namespace}'],
+//            ],
+//            full_command: [
+//                '${action.params.executable}',
+//                '${action.params.command}',
+//                '${prepareFlags(action.params.flags)}',
+//                '${action.params.name}',
+//            ],
+//        ],
+//        Kubectl_scale_down_up: [
+//            replicas_down: '0',
+//            replicas_up: '1',
+//        ],
+//        Kubectl_get_replicaset_name: [
+//            command: 'get replicaset',
+//            release_name: '${actions.Helm_status.release_name}',
+//            jsonpath: '\'{.items[0].metadata.name}\'',
+//            return_stdout: true,
+//            selector: 'release=${context.k8s.selector}',
+//            flags: [
+//                '--namespace': ['${action.params.namespace}'],
+//                '--selector': ['${action.params.selector}'],
+//                '-o': ['jsonpath=${action.params.jsonpath}'],
+//            ],
+//            full_command: [
+//                '${action.params.executable}',
+//                '${action.params.command}',
+//                '${prepareFlags(action.params.flags)}',
+//            ],
+//        ],
+//        Kubectl_get_pod_name: [
+//            command: 'get pod',
+//            release_name: '${actions.Helm_status.release_name}',
+//            jsonpath: '\'{.items[0].metadata.name}\'',
+//            return_stdout: true,
+//            selector: 'release=${context.k8s.selector}',
+//            flags: [
+//                '--namespace': ['${action.params.namespace}'],
+//                '--selector': ['${action.params.selector}'],
+//                '-o': ['jsonpath=${action.params.jsonpath}'],
+//            ],
+//            full_command: [
+//                '${action.params.executable}',
+//                '${action.params.command}',
+//                '${prepareFlags(action.params.flags)}',
+//            ],
+//        ],
+//        Kubectl_get_pods: [
+//            command: 'get pods',
+//            flags: [
+//                '--namespace': ['${action.params.namespace}'],
+//            ],
+//            full_command: [
+//                '${action.params.executable}',
+//                '${action.params.command}',
+//                '${prepareFlags(action.params.flags)}',
+//            ],
+//        ],
+//        Kubectl_get_loadbalancer_address: [
+//            command: 'get service',
+//            release_name: '${actions.Helm_status.release_name}',
+//            jsonpath: '\'{.items[0].status.loadBalancer.ingress[0].ip}:{.items[0].spec.ports[?(@.name=="http")].port}\'',
+//            return_stdout: true,
+//            selector: 'release=${context.k8s.selector}',
+//            flags: [
+//                '--namespace': ['${action.params.namespace}'],
+//                '--selector': ['${action.params.selector}'],
+//                '-o': ['jsonpath=${action.params.jsonpath}'],
+//            ],
+//            full_command: [
+//                '${action.params.executable}',
+//                '${action.params.command}',
+//                '${prepareFlags(action.params.flags)}',
+//            ],
+//        ],
+//        Kubectl_copy_from_pod: [
+//            command: 'cp',
+//            name: '',
+//            source_file_name: '',
+//            source: '${context.k8s.namespace}/${action.params.name}:${action.params.source_file_name}',
+//            destination: '',
+//            full_command: [
+//                '${action.params.executable}',
+//                '${action.params.command}',
+//                '${action.params.source}',
+//                '${action.params.destination}',
+//            ],
+//        ],
+//        Kubectl_get_pod_logs: [
+//            command: 'logs',
+//            name: '',
+//            flags: [
+//                '--namespace': ['${action.params.namespace}'],
+//            ],
+//            full_command: [
+//                '${action.params.executable}',
+//                '${action.params.command}',
+//                '${action.params.name}',
+//                '${prepareFlags(action.params.flags)}',
+//            ],
+//        ],
+//        HealthCheck_wait_http_ok: [
+//            action_timeout: 5,
+//            url: '',
+//            http_code: '200',
+//            interval: '5',
+//            command: '''bash -c 'while [[ "\\\$(curl -s -o /dev/null -w ''%{http_code}'' ${action.params.url})" != "${action.params.http_code}" ]]; do sleep ${action.params.interval}; done' ''',
+//            full_command: [
+//                '${action.params.command}',
+//            ],
+//        ],
     ],
 ]
