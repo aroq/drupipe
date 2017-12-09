9cef71cd88eb75754b54696f686aa56a646627f8
Alexander Tolstikov <tolstikov@gmail.com>
2017-12-09 20:17:17 +0300

    Add logger

--- src/com/github/aroq/drupipe/DrupipeLogger.groovy
+++ src/com/github/aroq/drupipe/DrupipeLogger.groovy
@@ -8,7 +8,7 @@ class DrupipeLogger implements Serializable {
 
     def logLevels
 
-    int logLevel
+    int logLevelWeight
 
     def logMessage(String logLevel, String message) {
         if (logLevels[logLevel].weight >= logLevel) {
