7578d09bb0c8e8fac19280ec04379287d9350f03
Alexander Tolstikov <tolstikov@gmail.com>
2017-12-09 21:50:27 +0300

    Add logger

--- src/com/github/aroq/drupipe/DrupipeKubernetesPod.groovy
+++ src/com/github/aroq/drupipe/DrupipeKubernetesPod.groovy
@@ -10,16 +10,14 @@ class DrupipeKubernetesPod extends DrupipeBase {
 
     ArrayList<DrupipeContainer> containers = []
 
-    DrupipeController controller
-
     boolean containerized = true
 
     boolean unipipe_retrieve_config = false
 
     def execute(body = null) {
-        controller.drupipeLogger.collapsedStart("POD EXECUTION START - ${name}")
-        controller.script.drupipeExecuteKubernetesContainers(containers, controller, unstash, stash, unipipe_retrieve_config)
-        controller.drupipeLogger.collapsedEnd()
+        executeWithCollapsed("POD: ${name}") {
+            controller.script.drupipeExecuteKubernetesContainers(containers, controller, unstash, stash, unipipe_retrieve_config)
+        }
     }
 
 }
