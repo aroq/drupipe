adddf7b08e48b132ea83c9f97d38119b9d95f8bf
Alexander Tolstikov <tolstikov@gmail.com>
2017-12-14 19:20:24 +0300

    Add support for missed classes

--- src/com/github/aroq/drupipe/actions/GitArtifact.groovy
+++ src/com/github/aroq/drupipe/actions/GitArtifact.groovy
@@ -3,7 +3,11 @@ package com.github.aroq.drupipe.actions
 class GitArtifact extends BaseAction {
 
     def retrieve() {
-        script.drupipeAction([action: "Git.clone", params: action.pipeline.context.builder.artifactParams << action.params], action.pipeline)
+        action.pipeline.executeAction(
+            action: 'Git.clone',
+            params: action.pipeline.context.builder.artifactParams << action.params,
+        )
+//        script.drupipeAction([action: "Git.clone", params: action.pipeline.context.builder.artifactParams << action.params], action.pipeline)
 
         def repoDir = action.params.dir + '/' + action.params.repoDirName
 
