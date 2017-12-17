1db51d739c2e39adec3da66190f8e2c859ec97b6
Alexander Tolstikov <tolstikov@gmail.com>
2017-12-17 14:40:53 +0300

    Add Commands.execute action

--- src/com/github/aroq/drupipe/processors/DrupipeFromProcessor.groovy
+++ src/com/github/aroq/drupipe/processors/DrupipeFromProcessor.groovy
@@ -63,12 +63,16 @@ class DrupipeFromProcessor implements Serializable, DrupipeProcessor {
     }
 
     def processFromItem(context, result, String from, String parent, String key = 'params') {
-        if (from == '.params.actions.JobDslSeed.perform') {
+        if (from == '.params.containers.common.operations.{$context.operations_type}') {
             drupipeLogger.trace "Process from: ${from}"
             drupipeLogger.trace "Process mode: ${mode}"
+            drupipeLogger.debugLog(context, controller.context, 'controller.context', [debugMode: 'json'], [], 'INFO')
         }
 
         from = controller.drupipeProcessorsController.drupipeParamProcessor.interpolateCommand(from, [:], controller.context)
+        if (from == '.params.containers.common.operations.{$context.operations_type}') {
+            drupipeLogger.trace "Process from: ${from} AFTER"
+        }
 
         def processorParams = collectKeyParamsFromJsonPath(context, from, 'processors')
         if (processorParams) {
