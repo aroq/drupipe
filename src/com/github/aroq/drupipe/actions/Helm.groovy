8a4951c1bd58066cddc76ac848c588b5ec62f51e
Alexander Tolstikov <tolstikov@gmail.com>
2017-12-21 12:55:30 +0300

    Refactor Helm apply

--- src/com/github/aroq/drupipe/actions/Helm.groovy
+++ src/com/github/aroq/drupipe/actions/Helm.groovy
@@ -11,6 +11,8 @@ echo "\${${action.params.secret_values_file_id}}" > .secret_values_file_id
             )
             action.params.secret_values_file = action.params.workingDir != '.' ? '.secret_values_file_id' : '.secret_values_file_id'
         }
+
+        action.params.namespace = action.params.namespace.replaceAll('/', '-')
     }
 
 }
