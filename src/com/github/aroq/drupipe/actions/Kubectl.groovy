673d4e86f1af2b594ce0a19291ad767f85c6d16a
Alexander Tolstikov <tolstikov@gmail.com>
2017-12-23 14:35:53 +0300

    Refactor Helm apply

--- src/com/github/aroq/drupipe/actions/Kubectl.groovy
+++ src/com/github/aroq/drupipe/actions/Kubectl.groovy
@@ -4,9 +4,9 @@ class Kubectl extends BaseShellAction {
 
     def scale_down_up() {
         def name = action.pipeline.executeAction(action: 'Kubectl.get_replicaset_name').stdout
-        script.warning "Replicaset name: ${name}"
-        script.warning "Replicaset replicas_down: ${action.params.replicas_down}"
-        script.warning "Replicaset replicas_up: ${replicas: action.params.replicas_up}"
+        action.pipeline.drupipeLogger.warning "Replicaset name: ${name}"
+        action.pipeline.drupipeLogger.warning "Replicaset replicas_down: ${action.params.replicas_down}"
+        action.pipeline.drupipeLogger.warning "Replicaset replicas_up: ${replicas: action.params.replicas_up}"
         action.pipeline.executeAction(
             action: 'Kubectl.scale_replicaset',
             params: [replicaset_name: name, replicas: action.params.replicas_down]
