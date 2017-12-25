6d1963594cf5d3f5c7ffa1237318ab28f8a2914e
Alexander Tolstikov <tolstikov@gmail.com>
2017-12-25 14:55:49 +0300

    Refactor Helm apply

--- src/com/github/aroq/drupipe/actions/Helm.groovy
+++ src/com/github/aroq/drupipe/actions/Helm.groovy
@@ -3,7 +3,6 @@ package com.github.aroq.drupipe.actions
 class Helm extends BaseShellAction {
 
     def apply_hook_pre_params() {
-        action.pipeline.drupipeLogger.trace "Inside appy_hook_preprocess()"
         if (action.pipeline.context.containerMode == 'kubernetes') {
             this.script.drupipeShell("""echo "\$${action.params.secret_values_file_id}" > .secret_values_file_id""", this.action.params)
             action.params.secret_values_file = '.secret_values_file_id'
@@ -13,7 +12,7 @@ class Helm extends BaseShellAction {
         }
     }
 
-    def apply_hook_pre_process() {
+    def apply_hook_post_params() {
         def files = []
         for (fileName in action.params.flags['-f']) {
             if (script.fileExists(fileName)) {
@@ -27,4 +26,3 @@ class Helm extends BaseShellAction {
     }
 
 }
-
