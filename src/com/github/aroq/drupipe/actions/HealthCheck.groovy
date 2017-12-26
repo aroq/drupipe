9919aa24e6332e33cb3aee511dab1457817a7c1c
Alexander Tolstikov <tolstikov@gmail.com>
2017-12-26 12:49:21 +0300

    Refactor Helm apply

--- src/com/github/aroq/drupipe/actions/HealthCheck.groovy
+++ src/com/github/aroq/drupipe/actions/HealthCheck.groovy
@@ -3,8 +3,6 @@ package com.github.aroq.drupipe.actions
 class HealthCheck extends BaseAction {
 
     def wait_http_ok() {
-        action.pipeline.drupipeLogger.warning "Kubectl_get_loadbalancer_address: ${action.pipeline.context.results.action.Kubectl_get_loadbalancer_address.url}"
-        action.pipeline.drupipeLogger.warning "HealthCheck url: ${action.params.url}"
         script.drupipeShell("${action.params.full_command.join(' ')}", action.params)
     }
 
