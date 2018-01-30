c021985808b5b9ed0776cd4b32269902adcb6188
Alexander Tolstikov <tolstikov@gmail.com>
2018-01-30 19:24:37 +0300

    Add uniconf test call

--- src/com/github/aroq/drupipe/DrupipeConfig.groovy
+++ src/com/github/aroq/drupipe/DrupipeConfig.groovy
@@ -45,7 +45,8 @@ class DrupipeConfig implements Serializable {
             this.script.sh("mkdir -p .unipipe")
             this.script.sh("mkdir -p .unipipe/temp")
 
-            this.script.sh(returnStdout: true, script: "#!/bin/sh -e\n" + '/uniconf/uniconf')
+            def result = this.script.sh(returnStdout: true, script: "#!/bin/sh -e\n" + '/uniconf/uniconf')
+            this.echo result
 
             params.debugEnabled = params.debugEnabled && params.debugEnabled != '0' ? true : false
 //            utils.dump(params, params, 'PIPELINE-PARAMS')
