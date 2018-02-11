e05666d6ceadedba52730b56dd74b0dc35f0c91c
Alexander Tolstikov <tolstikov@gmail.com>
2018-02-11 18:32:37 +0300

    Add uniconf test call

--- src/com/github/aroq/drupipe/DrupipeController.groovy
+++ src/com/github/aroq/drupipe/DrupipeController.groovy
@@ -56,10 +56,6 @@ class DrupipeController implements Serializable {
                             // Secret option for emergency remove workspace.
                             if (context.contexts.job) {
                                 def jobConfig = context.contexts.job
-                                if (jobConfig.context) {
-                                    script.echo "Merge Job context into config"
-                                    drupipeConfig.config = utils.merge(drupipeConfig.config, jobConfig.context)
-                                }
                                 archiveObjectJsonAndYaml(jobConfig, 'job')
                                 script.echo "Configuration end"
                                 drupipeLogger.debugLog(context, jobConfig, 'JOB', [debugMode: 'json'], [], 'INFO')
