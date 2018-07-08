4e9a2e330991947fe8fd46b90fe0744a5eb8098f
Alexander Tolstikov <tolstikov@gmail.com>
2018-07-08 16:18:18 +0300

    Fix skaffold

--- src/com/github/aroq/drupipe/actions/GCloud.groovy
+++ src/com/github/aroq/drupipe/actions/GCloud.groovy
@@ -5,7 +5,7 @@ import com.github.aroq.drupipe.DrupipeActionWrapper
 class GCloud extends BaseAction {
 
     def auth() {
-//        if (action.pipeline.context.containerMode != 'kubernetes') {
+        if (action.pipeline.context.containerMode != 'kubernetes') {
 
             // TODO: Make it centralized.
             String access_key_file
@@ -22,16 +22,20 @@ echo "\${${action.params.access_key_file_id}}" > .google_access_key_file
                 access_key_file = "\${${action.params.access_key_file_id}}"
             }
 
-            this.script.drupipeShell(
+            String command =
 """
 ${action.params.executable} auth activate-service-account --key-file ${access_key_file}
 ${action.params.executable} config set compute/zone ${action.params.compute_zone}
 ${action.params.executable} config set project ${action.params.project_name} 
 ${action.params.executable} config set container/use_client_certificate True
 ${action.params.executable} container clusters get-credentials ${action.params.cluster_name} 
-""", action.params)
+"""
+            script.echo command
 
-//        }
+
+            this.script.drupipeShell(command, action.params)
+
+        }
 
     }
 
