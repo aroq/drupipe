5c96f1efa52d69dba6ff991e3c9820eb56fe6d1d
Alexander Tolstikov <tolstikov@gmail.com>
2018-07-08 13:32:09 +0300

    Add skaffold

---
+++ src/com/github/aroq/drupipe/actions/Skaffold.groovy
@@ -0,0 +1,30 @@
+package com.github.aroq.drupipe.actions
+
+class Skaffold extends BaseShellAction {
+
+    def apply_hook_pre_params() {
+        if (action.pipeline.context.containerMode == 'kubernetes') {
+            if (action.params.secret_values_file_id) {
+                this.script.drupipeShell("""echo "\$${action.params.secret_values_file_id}" > .secret_values_file_id""", this.action.params)
+            }
+            action.params.secret_values_file = '.secret_values_file_id'
+        }
+        else {
+            action.params.secret_values_file = action.pipeline.context.env[action.params.secret_values_file_id]
+        }
+    }
+
+    def apply_hook_post_params() {
+        def files = []
+        for (fileName in action.params.flags['-f']) {
+            if (script.fileExists(fileName)) {
+                files.add fileName
+            }
+            else {
+                action.pipeline.drupipeLogger.warning "Helm values file does not exists: ${fileName}"
+            }
+        }
+        action.params.flags['-f'] = files
+    }
+
+}
