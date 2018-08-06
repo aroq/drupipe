b369582cda2d7adc51b66013e9db6c447d5d30d4
Alexander Tolstikov <tolstikov@gmail.com>
2018-08-06 14:24:46 +0300

    Remove debug

--- jobdsl/job_dsl_mothership.groovy
+++ jobdsl/job_dsl_mothership.groovy
@@ -88,13 +88,12 @@ println 'Servers: ' + servers.keySet().join(', ')
 def gitlabHelper = new GitlabHelper(script: this, config: configMain)
 
 projects.each { project ->
-    println "PROJECT PARAMS: ${project.value}"
+    println "PROJECT: ${project.value}"
     def config = configMain.clone()
     config = merge(config, project.value)
     def jenkins_servers
     if (config.params.jenkinsServers) {
         jenkins_servers = config.params.jenkinsServers
-        println "Jenkins servers: ${config.jenkins_servers}"
     }
     if (jenkins_servers && config.jenkinsServers.containsKey(config.env.drupipeEnvironment) && config.jenkinsServers[config.env.drupipeEnvironment].containsKey('tags') && jenkins_servers.intersect(config.jenkinsServers[config.env.drupipeEnvironment].tags)) {
         println "CONFIG projectConfigPath: ${config.projectConfigPath}"
