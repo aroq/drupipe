1aec4bd8d5545944f5bbfca22d132652804da6ff
Michael Molchanov <michael-molchanov@users.noreply.github.com>
2018-10-22 17:07:48 +0300

    Fix params and pathes in Behat action.

--- src/com/github/aroq/drupipe/actions/Behat.groovy
+++ src/com/github/aroq/drupipe/actions/Behat.groovy
@@ -3,14 +3,14 @@ package com.github.aroq.drupipe.actions
 class Behat extends BaseAction {
 
     def perform() {
-        def testEnvironment = action.params.testEnvironment ? action.params.testEnvironment : action.pipeline.context.environment
+        def testEnvironment = action.pipeline.context.env.testEnvironment ? action.pipeline.context.env.testEnvironment : action.pipeline.context.environment
         def features = ''
-        if (action.params.features) {
-            features = action.params.features
+        if (action.pipeline.context.env.features) {
+            features = action.pipeline.context.env.features
         }
         def tags = ''
-        if (action.pipeline.context.tags) {
-            tags = "--tags=${action.pipeline.context.tags}"
+        if (action.pipeline.context.env.tags) {
+            tags = "--tags=${action.pipeline.context.env.tags}"
         }
 
         // TODO: Add settings to exit with error on Behat errors.
@@ -18,7 +18,7 @@ class Behat extends BaseAction {
             if (script.fileExists("${action.params.masterPath}/${action.params.pathToEnvironmentConfig}/behat.${testEnvironment}.yml")) {
                 script.drupipeShell(
                     """
-                cd ${action.params.masterPath}/${action.pipeline.context.docrootDir}
+                cd ${action.params.masterPath}
                 mkdir -p ${action.params.workspaceRelativePath}/reports
                 ${action.params.masterRelativePath}/${action.params.behatExecutable} --config=${action.params.masterRelativePath}/${action.params.pathToEnvironmentConfig}/behat.${testEnvironment}.yml ${action.params.behat_args} --out=${action.params.workspaceRelativePath}/reports ${tags} ${features}
             """, action.params
