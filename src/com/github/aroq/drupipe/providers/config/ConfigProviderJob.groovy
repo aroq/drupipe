8c4147fc0fbc09546c9b172df11559607e083901
Alexander Tolstikov <tolstikov@gmail.com>
2019-01-15 18:47:57 +0300

    Debug

--- src/com/github/aroq/drupipe/providers/config/ConfigProviderJob.groovy
+++ src/com/github/aroq/drupipe/providers/config/ConfigProviderJob.groovy
@@ -11,9 +11,9 @@ class ConfigProviderJob extends ConfigProviderBase {
 
 //        String prefixPath = script.env.JENKINS_HOME + "/config_cache/" + script.env.JOB_NAME
         String prefixPath = script.env.JENKINS_HOME + "/config_cache/cicd-k8s-infra/seed"
-        String jobConfigFileName = prefixPath + "/ConfigProviderJob.yaml"
-//        def sourceDir = drupipeConfig.drupipeSourcesController.sourceDir(drupipeConfig.config, 'project')
-//        String jobConfigFileName = sourceDir + "/scenarios/test/ConfigProviderJob.yaml"
+//        String jobConfigFileName = prefixPath + "/ConfigProviderJob.yaml"
+        def sourceDir = drupipeConfig.drupipeSourcesController.sourceDir(drupipeConfig.config, 'project')
+        String jobConfigFileName = sourceDir + "/scenarios/test/ConfigProviderJob.yaml"
 
         if (this.script.fileExists(jobConfigFileName)) {
             script.echo "Cached ConfigProviderJob is found, loading..."
