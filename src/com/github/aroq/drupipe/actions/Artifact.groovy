3197b802814a373f0ec740c72d649c234e4754bd
Michael Molchanov <mmolchanov@adyax.com>
2019-03-04 12:52:12 +0000

    Add s3upload action.

--- src/com/github/aroq/drupipe/actions/Artifact.groovy
+++ src/com/github/aroq/drupipe/actions/Artifact.groovy
@@ -20,5 +20,29 @@ class Artifact extends BaseAction {
         this.script.archiveArtifacts artifacts: "${artifact_path}/**"
     }
 
+    def s3Upload() {
+        def s3_credentials_id = (action.params.s3_credentials_id && action.params.s3_credentials_id.length() != 0) ? "${action.params.s3_credentials_id}" : false
+        def s3_region = (action.params.s3_region && action.params.s3_region.length() != 0) ? "${action.params.s3_region}" : false
+        def s3_bucket = (action.params.s3_bucket && action.params.s3_bucket.length() != 0) ? "${action.params.s3_bucket}" : false
+        def s3_path = (action.params.s3_path && action.params.s3_path.length() != 0) ? "${action.params.s3_path}" : "index.html"
+        def artifact_path = (action.params.artifact_path && action.params.artifact_path.length() != 0) ? "${action.params.artifact_path}" : "reports"
+
+        try {
+            if (s3_credentials_id == false || s3_region == false || s3_bucket == false) {
+                throw new Exception("Check credentials, region and bucket settings.")
+            }
+            else {
+                this.script.withAWS(credentials: s3_credentials_id, region: s3_region) {
+                    this.script.s3Upload(bucket:s3_bucket, path: s3_path, file: artifact_path)
+                }
+            }
+        }
+        catch (e) {
+            this.script.echo ex.getMessage()
+            this.script.archiveArtifacts artifacts: "${artifact_path}/**"
+        }
+
+        return [result: '']
+    }
 }
 
