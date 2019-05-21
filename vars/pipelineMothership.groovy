1e971780672835894061561ac4c19b25ea084149
Michael Molchanov <mmolchanov@adyax.com>
2019-05-21 13:28:56 +0000

    Fix mothership crash.

--- vars/pipelineMothership.groovy
+++ vars/pipelineMothership.groovy
@@ -1,7 +1,7 @@
 import com.github.aroq.drupipe.DrupipeController
 
 def call() {
-    (new DrupipeController([script: this, params: params])).execute()
+    (new DrupipeController([script: this, params: params, env: env])).execute()
 //    drupipe { pipeline ->
 //        drupipeBlock(nodeName: 'master', pipeline) {
 //            drupipeStage('seed', pipeline) {
