221948b3416117f993c579606fc574b0001e5c5f
Michael Molchanov <mmolchanov@adyax.com>
2019-05-29 13:47:38 +0000

    Fix technical params filter for notifications.

--- src/com/github/aroq/drupipe/Utils.groovy
+++ src/com/github/aroq/drupipe/Utils.groovy
@@ -248,7 +248,7 @@ def paramsMarkdownTable(jenkinsParams) {
         table = table + "|:---|:---|\n"
 
         jenkinsParams.each {param, value ->
-            if (!(param =~ $/^(separator|variant)/$)) {
+            if (!(param =~ $/(?i)^(separator|variant)/$)) {
                 table = table + "|${param}|`${value}`|\n"
             }
         }
