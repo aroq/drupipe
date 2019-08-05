8f9a0b5900368d5c300313ba80fac1a09b2bee70
Michael Molchanov <mmolchanov@adyax.com>
2019-08-05 13:37:26 +0000

    Add allure action without entrypoint.

--- src/com/github/aroq/drupipe/actions/AllureTester.groovy
+++ src/com/github/aroq/drupipe/actions/AllureTester.groovy
@@ -20,4 +20,24 @@ class AllureTester extends BaseAction {
 
         script.archiveArtifacts artifacts: "${report_dir}/**"
     }
+
+    def test2() {
+
+        def execute = (action.pipeline.context.env.allure_execute && action.pipeline.context.env.allure_execute.length() != 0) ? action.pipeline.context.env.allure_execute : ''
+        def report_dir = (action.pipeline.context.env.allure_report_dir && action.pipeline.context.env.allure_report_dir.length() != 0) ? action.pipeline.context.env.allure_report_dir : ''
+
+        script.dir(report_dir) {
+            this.script.deleteDir()
+        }
+
+        script.drupipeShell(
+"""
+${action.pipeline.context.workspace}/${execute}
+""", action.params)
+
+        script.allure includeProperties: false, jdk: '', properties: [], reportBuildPolicy: 'ALWAYS', report: "${report_dir}/generated-report", results: [[path: report_dir]]
+
+        // script.archiveArtifacts artifacts: "${report_dir}/**"
+    }
+
 }
