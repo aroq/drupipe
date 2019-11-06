6ae2c4b90c125ed50d40cf758cb239f2c5e400dd
Alexander Tolstikov <atolstikov@adyax.com>
2019-11-06 15:54:32 +0300

    Change env vars management for k8s

--- vars/drupipeExecuteKubernetesPod.groovy
+++ vars/drupipeExecuteKubernetesPod.groovy
@@ -30,13 +30,13 @@ def call(DrupipePod pod, ArrayList unstash = [], ArrayList stash = [], unipipe_r
 
 
     def env_vars
-    env_vars << new EnvVar(key: 'TF_VAR_consul_address', value: controller.context.env.TF_VAR_consul_address),
-    env_vars <<  new EnvVar(key: 'UNIPIPE_SOURCES', value: controller.context.env.UNIPIPE_SOURCES),
-    env_vars <<  new SecretEnvVar(key: 'DIGITALOCEAN_TOKEN', secretName: 'zebra-keys', secretKey: 'zebra_do_token'),
-    env_vars <<  new SecretEnvVar(key: 'ANSIBLE_VAULT_PASS_FILE', secretName: 'zebra-keys', secretKey: 'zebra_ansible_vault_pass'),
-    env_vars <<  new SecretEnvVar(key: 'GITLAB_API_TOKEN_TEXT', secretName: 'zebra-keys', secretKey: 'zebra_gitlab_api_token'),
-    env_vars << new SecretEnvVar(key: 'GCLOUD_ACCESS_KEY', secretName: 'zebra-keys', secretKey: 'zebra_gcloud_key'),
-    env_vars << new SecretEnvVar(key: 'HELM_ZEBRA_SECRETS_FILE', secretName: 'zebra-keys', secretKey: 'zebra_cicd_helm_secret_values'),
+    env_vars << new EnvVar(key: 'TF_VAR_consul_address', value: controller.context.env.TF_VAR_consul_address)
+    env_vars <<  new EnvVar(key: 'UNIPIPE_SOURCES', value: controller.context.env.UNIPIPE_SOURCES)
+    env_vars <<  new SecretEnvVar(key: 'DIGITALOCEAN_TOKEN', secretName: 'zebra-keys', secretKey: 'zebra_do_token')
+    env_vars <<  new SecretEnvVar(key: 'ANSIBLE_VAULT_PASS_FILE', secretName: 'zebra-keys', secretKey: 'zebra_ansible_vault_pass')
+    env_vars <<  new SecretEnvVar(key: 'GITLAB_API_TOKEN_TEXT', secretName: 'zebra-keys', secretKey: 'zebra_gitlab_api_token')
+    env_vars << new SecretEnvVar(key: 'GCLOUD_ACCESS_KEY', secretName: 'zebra-keys', secretKey: 'zebra_gcloud_key')
+    env_vars << new SecretEnvVar(key: 'HELM_ZEBRA_SECRETS_FILE', secretName: 'zebra-keys', secretKey: 'zebra_cicd_helm_secret_values')
     for (def i = 0; i < 3; i++) {
         env_vars << new SecretEnvVar(key: "DIGITALOCEAN_TOKEN-${i}", secretName: 'zebra-keys', secretKey: 'zebra_do_token')
     }
